2c7b1a1132274d475170a1565d6894e3acf2cb40
Peter Pistorius <peter.pistorius@gmail.com>
2016-03-18 12:08:21 +0100

    generating boilerplate action types.

--- src/schedule/ScheduleConstants.js
+++ src/schedule/ScheduleConstants.js
@@ -1,24 +1,4 @@
+import { generateActionNames } from '../utils';
 
-// TODO: Make CRUD generator
-export default {
-  SCHEDULE_FETCH: [
-    'schedule.fetch.request',
-    'schedule.fetch.success',
-    'schedule.fetch.failure',
-  ],
-  SCHEDULE_ADD: [
-    'schedule.add.request',
-    'schedule.add.success',
-    'schedule.add.failure',
-  ],
-  SCHEDULE_EDIT: [
-    'schedule.edit.request',
-    'schedule.edit.success',
-    'schedule.edit.failure',
-  ],
-  SCHEDULE_DELETE: [
-    'schedule.delete.request',
-    'schedule.delete.success',
-    'schedule.delete.failure',
-  ],
-};
+const actionTypes = generateActionNames('schedule');
+export default actionTypes;
