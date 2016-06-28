0724e503b5695a5ca42e0d18eefc4601a1ac6031
Peter Pistorius <peter.pistorius@gmail.com>
2016-06-28 09:32:13 +0200

    added route.

---
+++ src/Mentor/ForgotPassword.js
@@ -0,0 +1,123 @@
+/* eslint-disable react/jsx-no-bind */
+import React, {
+  PropTypes,
+  Component,
+} from 'react';
+
+import {
+  View,
+} from 'react-native';
+
+import {
+  Heading,
+  Text,
+  TextLink,
+  TextInput,
+  Button,
+} from '../Components';
+
+
+import { connect } from 'react-redux';
+
+class ForgotPassword extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newPassword: '123',
+      newPasswordRepeat: '123',
+      isLoading: false,
+      errors: {},
+    };
+
+    this.validateInput = this.validateInput.bind(this);
+    this.onSubmitPress = this.onSubmitPress.bind(this);
+  }
+
+  onSubmitPress() {
+    this.validateInput();
+    if (this.state.errors.length > 0) {
+      return;
+    }
+
+    // callAPI('/mentor/reset-password/', this.props.authorizationToken, {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     password: this.state.newPassword,
+    //   }),
+    // })
+    // .then(data => {
+    //   console.log('success', data);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+  }
+
+  validateInput() {
+    const errors = {};
+    const { newPassword, newPasswordRepeat } = this.state;
+    if (newPassword.length <= 0) {
+      errors.newPassword = 'Passwords can not be blank';
+    }
+    if (newPassword !== newPasswordRepeat) {
+      errors.newPasswordRepeat = 'Passwords do not match.';
+    }
+    this.setState({ errors });
+  }
+
+  render() {
+    const isLoadingPartial = this.state.isLoading ? <Text>Loading</Text> : null;
+    return (
+      <View>
+        {isLoadingPartial}
+        <TextInput
+          placeholder="New password"
+          value={this.state.newPassword}
+          onChangeText={ (newPassword) => {
+            this.setState({ newPassword });
+            this.validateInput();
+          }}
+        />
+        <Text>{ this.state.errors.newPassword }</Text>
+
+        <TextInput
+          placeholder="Repeat Password"
+          value={this.state.newPasswordRepeat}
+          onChangeText={ (newPasswordRepeat) => {
+            this.setState({ newPasswordRepeat });
+            this.validateInput();
+          }}
+        />
+
+      <Text>{ this.state.errors.network }</Text>
+
+      <Text>{ this.state.errors.newPasswordRepeat }</Text>
+
+        <TouchableHighlight onPress={this.onSubmitPress}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+ResetPassword.propTypes = {
+  authToken: PropTypes.string.isRequired,
+  onResetPassword: PropTypes.func.isRequired,
+};
+
+
+const mapStateToProps = function mapStateToProps(state) {
+  return {
+    authToken: state.mentor.authToken,
+  };
+};
+
+ResetPassword = connect(
+  mapStateToProps,
+  {}
+)(ResetPassword);
+
+
+export default ResetPassword;
