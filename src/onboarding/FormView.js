3840a21cc2fafde2f7b0a543c30f1b907f69cd76
Peter Pistorius <peter.pistorius@gmail.com>
2016-07-06 16:19:43 +0200

    finished form generator.

--- src/onboarding/FormView.js
+++ src/onboarding/FormView.js
@@ -1,39 +1,56 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { Header, TextInput, NavigationBar } from 'src/components';
+import { Header, NavigationBar } from 'src/components';
 
 
+// TODO: Some inputs have hints.
 class FormView extends React.Component {
 
   constructor({
     title,
-    inputFields,
+    inputFields = {},
     navigationBarProps,
   }) {
     super();
-    this.inputs = inputFields;
+
+
+    this.inputFields = inputFields;
     this.title = title;
     this.navigationBarProps = navigationBarProps;
+
+    // Create a state object based off of the keys in inputFields; the state
+    // holds the `text` that's entered into the inputs.
+    const tmpState = {};
+    Object.keys(this.inputFields).forEach((key) => { tmpState[key] = ''; });
+    this.state = tmpState;
   }
 
+  inputFieldsEmpty() {
+    return Object.keys(this.state).some((key) => this.state[key].length === 0);
+  }
 
   render() {
+    const fields = Object.keys(this.inputFields).map((key, index) => {
+      const field = this.inputFields[key];
+      return (
+        <field.component {...field.componentProps}
+          key={`input-${index}`}
+          value={this.state[key]}
+          onChangeText={(text) => this.setState({ [key]: text })}
+        />
+      );
+    });
+
+
     return (
       <View style={{ flex: 1 }}>
         <Header title={this.props.title} />
-        {this.inputs.map((input, key) => (
-          <input.component
-            placeholder={input.placeholder}
-            onChangeText={(text) => this.setState({ key: text })}
-            value={this.state[key]}
-          />
-        ))}
-        <TextInput placeholder="Type your answer here"
-          onChangeText={(motivation) => this.setState({ motivation })}
-          value={this.state.motivation}
+        {fields}
+        <NavigationBar
+          nextEnabled={!this.inputFieldsEmpty()}
+          {...this.navigationBarProps}
         />
-      <NavigationBar {...this.props.navigationBarProps} to="own-mentors" />
       </View>
     );
   }
@@ -41,8 +58,8 @@ class FormView extends React.Component {
 
 FormView.propTypes = {
   title: React.PropTypes.string.isRequired,
-  inputs: React.PropTypes.object.isRequired,
+  inputFields: React.PropTypes.object.isRequired,
   navigationBarProps: React.PropTypes.object,
 };
 
-export default MoFormViewtivation;
+export default FormView;
--- src/onboarding/Route.js
+++ src/onboarding/Route.js
@@ -5,8 +5,9 @@ import { Router, Route, StackRoute, IndexRoute } from 'react-router-native';
 import Welcome from './Welcome'
 import ProfilePicture from './ProfilePicture';
 import CameraRoll from './CameraRoll';
-import Job from './Job';
 import Motivation from './Motivation';
+
+import FormView from './FormView';
 
 
 const StackContainer = (props) => (
@@ -15,13 +16,44 @@ const StackContainer = (props) => (
   </View>
 );
 
+import { TextInput } from 'src/components';
+
+const Job = () => {
+
+  const inputFields = {
+    jobTitle: {
+      component: TextInput,
+      componentProps: {
+        placeholder: 'Job Title',
+      },
+    },
+    jobSector: {
+      component: TextInput,
+      componentProps: {
+        placeholder: 'Job Sector',
+      },
+    },
+    navigationBarProps: {
+      to: 'motivation',
+    },
+  };
+
+
+  return <FormView
+    title="What do you do?"
+    inputFields={inputFields}
+    />
+}
+
+
+
 const onboardingRoute = (
 
   <StackRoute
     path="/"
     component={StackContainer}
   >
-    <IndexRoute component={Motivation} />
+    <IndexRoute component={Job} />
     <Route path="profile-picture" component={ProfilePicture} />
     <Route path="camera-roll" component={CameraRoll} />
     <Route path="job" component={Job} />
