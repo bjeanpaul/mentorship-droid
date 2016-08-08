ccbadbf782df4b85789487ecd0e02bf170c29795
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-08 14:29:09 +0200

    removed withRouter from container components.

--- src/auth/LoginContainer.js
+++ src/auth/LoginContainer.js
@@ -1,20 +1,19 @@
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 
 import { login } from './actions';
 import Login from './Login';
 
-const mapDispatchToProps = function mapDispatchToProps(dispatch, { router }) {
+const mapDispatchToProps = function mapDispatchToProps(dispatch) {
   return {
     handleLoginPress: (email, password) => {
       dispatch(login(email, password, () => {
-        router.push('/hello');
+        // TODO: Transition to new view.
       }));
     },
   };
 };
 
-export default withRouter(connect(
+export default connect(
   (state) => state.auth,
   mapDispatchToProps
-)(Login));
+)(Login);
