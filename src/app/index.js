03dbc6381dfa3cd2d31960951be399c749d527b7
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-10 10:32:35 +0200

    fixed profile picture.

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'src/stores/configureStore';
 const store = configureStore();
 
-import Component from 'src/profile/Hello';
+import Component from 'src/profile/ProfilePicture';
 
 const App = function App() {
   return (
---
+++ src/profile/ProfilePicture.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+import { BaseView, Toolbar, Text } from 'src/components';
+
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 120,
+    height: 120,
+    marginBottom: 19,
+  },
+  imageLabel: {
+    color: '#9fb1b3',
+    fontSize: 18,
+  },
+});
+
+// TODO: Picture path should be dynamic.
+const ProfilePicture = ({
+}) => {
+  const IMG_PROFILE_ADD = require('app/assets/Profile_Add.png');
+  return (
+    <BaseView>
+      <Toolbar title="Add a profile picture" />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={IMG_PROFILE_ADD}
+        />
+        <Text style={styles.imageLabel}>Choose Photo</Text>
+      </View>
+
+    </BaseView>
+  );
+};
+ProfilePicture.propTypes = {
+  picturePath: React.PropTypes.string,
+};
+
+export default ProfilePicture;
