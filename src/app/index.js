7d86b14ca02b4379752e3a0503b1e94a5fef459c
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-10 18:38:52 +0200

    removing component from index.js

--- src/app/index.js
+++ src/app/index.js
@@ -6,12 +6,10 @@ import { Provider } from 'react-redux';
 import configureStore from 'src/stores/configureStore';
 const store = configureStore();
 
-import Component from 'src/call/CallNotification';
-
 const App = function App() {
   return (
     <Provider store={store}>
-      <Component date="sadsadsad sa" activityName="asdsadsad sadsa d asd" />
+      <View />
     </Provider>
   );
 };
