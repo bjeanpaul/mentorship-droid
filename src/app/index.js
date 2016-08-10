9a3e3d413f4ba4ceb7521fc083651d55efa18e40
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-10 13:25:59 +0200

    moved styles to constants.

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
+
 
 import configureStore from 'src/stores/configureStore';
 const store = configureStore();
 
-import Component from 'src/profile/ProfilePicture';
 
 const App = function App() {
   return (
     <Provider store={store}>
-      <Component />
+      <View />
     </Provider>
   );
 };
