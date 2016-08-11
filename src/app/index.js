b91a892156d13cecbfd114b9d96a9febdc47a03d
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-11 19:39:34 +0200

    fix linting.

--- src/app/index.js
+++ src/app/index.js
@@ -6,27 +6,10 @@ import { Provider } from 'react-redux';
 import configureStore from 'src/stores/configureStore';
 const store = configureStore();
 
-import C from 'src/schedule/ScheduleList';
-
 const App = function App() {
   return (
     <Provider store={store}>
-      <C
-        calls={[
-          {
-            date: '2016-08-11',
-            activityName: 'Problem',
-          },
-          {
-            date: '2016-08-12',
-            activityName: 'Sausage',
-          },
-          {
-            date: '2016-08-13',
-            activityName: 'Test',
-          },
-        ]}
-      />
+      <View />
     </Provider>
   );
 };
