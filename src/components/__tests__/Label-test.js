c63e7a9a8a2a57227c7ea2a49ce92ab46168b22d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-15 15:56:52 +0200

    Add tests for Label component

---
+++ src/components/__tests__/Label-test.js
@@ -0,0 +1,27 @@
+jest
+  .unmock('src/components/Label')
+
+import React from 'react';
+import { Text } from 'react-native';
+import { shallow } from 'enzyme';
+import Label from 'src/components/Label';
+
+
+describe('Label', () => {
+  it('should render its title', () => {
+    const el = shallow(<Label title="foo" />);
+    expect(el.children().text()).toEqual("foo")
+  });
+
+  fit('should support style overriding', () => {
+    const el = shallow(
+      <Label
+        title="foo"
+        style={{fontSize: 23}}
+      />);
+
+      expect(el.props()).toEqual(jasmine.objectContaining({
+        style: jasmine.arrayContaining([{fontSize: 23}])
+      }))
+  });
+});
