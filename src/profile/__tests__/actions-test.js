09ac4fdb396b4156b6a4c45530a231e11255355a
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-17 19:46:32 +0200

    Add fetchProfile() action creator

--- src/profile/__tests__/actions-test.js
+++ src/profile/__tests__/actions-test.js
@@ -1,30 +1,77 @@
 jest.mock('src/api/profiles');
 
 import { capture } from 'scripts/helpers';
-import { uploadProfileImage } from 'src/profile/actions';
 import { fakeContext } from 'scripts/helpers';
 import { noop } from 'lodash';
+import * as api from 'src/api';
 
 import {
-  ApiResponseError,
-  uploadProfileImage as apiUploadProfileImage
-} from 'src/api';
-
+  fetchProfile,
+  uploadProfileImage,
+} from 'src/profile/actions';
 
 import {
+  PROFILE_FETCH_REQUEST,
+  PROFILE_FETCH_SUCCESS,
+  PROFILE_FETCH_FAILURE,
+
   PROFILE_IMAGE_UPDATE_REQUEST,
   PROFILE_IMAGE_UPDATE_SUCCESS,
   PROFILE_IMAGE_UPDATE_FAILURE,
 } from 'src/profile/constants';
 
+const { ApiResponseError } = api;
+
 
 describe('profile/actions', () => {
   beforeEach(() => {
-    apiUploadProfileImage.mockClear();
-    apiUploadProfileImage.mockReturnValue(Promise.resolve(null));
+    api.getProfile.mockClear();
+    api.getProfile.mockReturnValue(Promise.resolve({ result: 23 }));
+
+    api.uploadProfileImage.mockClear();
+    api.uploadProfileImage.mockReturnValue(Promise.resolve(null));
   });
 
-  fdescribe('uploadProfileImage', () => {
+  describe('fetchProfile', () => {
+    it('should dispatch request', async () => {
+      const [action] = await capture(fetchProfile(23), fakeContext());
+
+      expect(action).toEqual({
+        type: PROFILE_FETCH_REQUEST,
+      });
+    });
+
+    it('should dispatch success for successful fetches', async () => {
+      api.getProfile.mockReturnValue(Promise.resolve({ result: 23 }));
+
+      const [_request, action] = await capture(fetchProfile(23), fakeContext());
+
+      expect(action).toEqual({
+        type: PROFILE_FETCH_SUCCESS,
+        payload: { entities: { result: 23 } },
+      });
+    });
+
+    it('should dispatch failure for failed fetches', async () => {
+      api.getProfile.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
+
+      const [_request, action] = await capture(fetchProfile(23), fakeContext());
+
+      expect(action).toEqual({
+        type: PROFILE_FETCH_FAILURE,
+      });
+    });
+
+    it('should call api.getProfile() with the correct params', async () => {
+      const ctx = fakeContext();
+      const { auth } = ctx;
+
+      await fetchProfile(23)(noop, ctx);
+      expect(api.getProfile.mock.calls).toEqual([[23, auth]]);
+    });
+  });
+
+  describe('uploadProfileImage', () => {
     it('should dispatch request', async () => {
       const ctx = fakeContext();
       const [action] = await capture(uploadProfileImage(23, 'foo.png'), ctx);
@@ -35,7 +82,7 @@ describe('profile/actions', () => {
     });
 
     it('should dispatch success for successful uploads', async () => {
-      apiUploadProfileImage.mockReturnValue(Promise.resolve(null));
+      api.uploadProfileImage.mockReturnValue(Promise.resolve(null));
 
       const ctx = fakeContext();
       const [_request, action] = await capture(uploadProfileImage(23, 'foo.png'), ctx);
@@ -46,7 +93,7 @@ describe('profile/actions', () => {
     });
 
     it('should dispatch failure for failed uploads', async () => {
-      apiUploadProfileImage.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
+      api.uploadProfileImage.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
 
       const ctx = fakeContext();
       const [_request, action] = await capture(uploadProfileImage(23, 'foo.png'), ctx);
@@ -56,12 +103,12 @@ describe('profile/actions', () => {
       });
     });
 
-    it('should call apiUploadProfileImage() with the correct params', async () => {
+    it('should call api.uploadProfileImage() with the correct params', async () => {
       const ctx = fakeContext();
       const { auth } = ctx;
 
       await uploadProfileImage(23, 'foo.png')(noop, ctx);
-      expect(apiUploadProfileImage.mock.calls).toEqual([[23, 'foo.png', auth]]);
+      expect(api.uploadProfileImage.mock.calls).toEqual([[23, 'foo.png', auth]]);
     });
   });
 });
--- src/profile/actions.js
+++ src/profile/actions.js
@@ -1,12 +1,41 @@
 import { trap } from 'src/helpers';
-import { ApiResponseError } from 'src/api';
-import { uploadProfileImage as apiUploadProfileImage } from 'src/api';
+import * as api from 'src/api';
 
 import {
+  PROFILE_FETCH_REQUEST,
+  PROFILE_FETCH_SUCCESS,
+  PROFILE_FETCH_FAILURE,
+
   PROFILE_IMAGE_UPDATE_REQUEST,
   PROFILE_IMAGE_UPDATE_SUCCESS,
   PROFILE_IMAGE_UPDATE_FAILURE,
 } from 'src/profile/constants';
+
+const { ApiResponseError } = api;
+
+
+const fetchProfileRequest = () => ({
+  type: PROFILE_FETCH_REQUEST,
+});
+
+
+const fetchProfileSuccess = entities => ({
+  type: PROFILE_FETCH_SUCCESS,
+  payload: { entities },
+});
+
+
+const fetchProfileFailure = () => ({
+  type: PROFILE_FETCH_FAILURE,
+});
+
+
+const fetchProfile = id => (dispatch, { auth }) => Promise.resolve()
+  .then(() => fetchProfileRequest())
+  .then(dispatch)
+  .then(() => api.getProfile(id, auth))
+  .then(fetchProfileSuccess, trap(ApiResponseError, fetchProfileFailure))
+  .then(dispatch);
 
 
 const uploadProfileImageRequest = () => ({
@@ -27,11 +56,12 @@ const uploadProfileImageFailure = () => ({
 const uploadProfileImage = (id, path) => (dispatch, { auth }) => Promise.resolve()
   .then(() => uploadProfileImageRequest())
   .then(dispatch)
-  .then(() => apiUploadProfileImage(id, path, auth))
+  .then(() => api.uploadProfileImage(id, path, auth))
   .then(uploadProfileImageSuccess, trap(ApiResponseError, uploadProfileImageFailure))
   .then(dispatch);
 
 
 export {
+  fetchProfile,
   uploadProfileImage,
 };
