8241d0af12f3b362fd02dba0a8f70678257454e3
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-17 12:51:21 +0200

    Use context middleware in store

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -3,15 +3,17 @@ import thunkMiddleware from 'redux-thunk';
 import merge from 'lodash/merge';
 
 import HTTPRequestMiddleware from './HTTPRequestMiddleware';
-import { getAuthorizationToken as getAuthorizationHeaderValue }
-  from 'src/configuration';
+import { getAuthorizationToken as getAuthorizationHeaderValue } from 'src/configuration';
+
+import auth from 'src/auth/reducer';
+import { getContext } from 'src/stores/helpers';
+import initialState from 'src/stores/initalState';
+import contextMiddleware from 'src/contextMiddleware';
+
 const httpRequestMiddleware = new HTTPRequestMiddleware({
   getAuthorizationHeaderValue,
 });
 
-import auth from 'src/auth/reducer';
-
-import initialState from 'src/stores/initalState';
 
 const entities = (state = {}, action) => {
   if (action.payload && action.payload.entities) {
@@ -31,7 +33,8 @@ export default function configureStore() {
     initialState(),
     applyMiddleware(
       thunkMiddleware,
-      httpRequestMiddleware.apply
+      httpRequestMiddleware.apply,
+      contextMiddleware(getContext),
     )
   );
 }
