a90cb943cc9ac9cf3baf85e11cf9360b0ac2c008
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-18 13:57:20 +0200

    added placeholder.

--- src/onboarding/LandingContainer.js
+++ src/onboarding/LandingContainer.js
@@ -3,10 +3,9 @@ import Landing from './Landing';
 
 // TODO: Import Navigation Actions.
 const todo = () => {
-  console.log('do me.')
   return {
     type: 'TODO',
-  }
+  };
 };
 
 export default connect(
