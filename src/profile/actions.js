5a5927a90353366eb1051986622552bec8f03be5
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-18 22:36:29 +0200

    Use action helpers for profile actions

--- src/profile/actions.js
+++ src/profile/actions.js
@@ -2,77 +2,37 @@ import { trap } from 'src/helpers';
 import * as api from 'src/api';
 import * as constants from 'src/profile/constants';
 
+import {
+  apiAction,
+  dataAction,
+  staticAction,
+} from 'src/actionHelpers';
+
 const { ApiResponseError } = api;
 
 
-const fetchProfileRequest = () => ({
-  type: constants.PROFILE_FETCH_REQUEST,
+const fetchProfile = apiAction({
+  method: api.getProfile,
+  request: staticAction(constants.PROFILE_FETCH_REQUEST),
+  success: dataAction(constants.PROFILE_FETCH_SUCCESS),
+  failures: [[ApiResponseError, staticAction(constants.PROFILE_FETCH_FAILURE)]],
 });
 
 
-const fetchProfileSuccess = data => ({
-  type: constants.PROFILE_FETCH_SUCCESS,
-  payload: data,
+const updateProfile = apiAction({
+  method: api.updateProfile,
+  request: staticAction(constants.PROFILE_UPDATE_REQUEST),
+  success: staticAction(constants.PROFILE_UPDATE_SUCCESS),
+  failures: [[ApiResponseError, staticAction(constants.PROFILE_UPDATE_FAILURE)]],
 });
 
 
-const fetchProfileFailure = () => ({
-  type: constants.PROFILE_FETCH_FAILURE,
+const uploadProfileImage = apiAction({
+  method: api.uploadProfileImage,
+  request: staticAction(constants.PROFILE_IMAGE_UPDATE_REQUEST),
+  success: staticAction(constants.PROFILE_IMAGE_UPDATE_SUCCESS),
+  failures: [[ApiResponseError, staticAction(constants.PROFILE_IMAGE_UPDATE_FAILURE)]],
 });
-
-
-const fetchProfile = id => (dispatch, { auth }) => Promise.resolve()
-  .then(() => fetchProfileRequest())
-  .then(dispatch)
-  .then(() => api.getProfile(id, auth))
-  .then(fetchProfileSuccess, trap(ApiResponseError, fetchProfileFailure))
-  .then(dispatch);
-
-
-const updateProfileRequest = () => ({
-  type: constants.PROFILE_UPDATE_REQUEST,
-});
-
-
-const updateProfileSuccess = () => ({
-  type: constants.PROFILE_UPDATE_SUCCESS,
-});
-
-
-const updateProfileFailure = () => ({
-  type: constants.PROFILE_UPDATE_FAILURE,
-});
-
-
-const updateProfile = (id, data) => (dispatch, { auth }) => Promise.resolve()
-  .then(() => updateProfileRequest())
-  .then(dispatch)
-  .then(() => api.updateProfile(id, data, auth))
-  .then(updateProfileSuccess, trap(ApiResponseError, updateProfileFailure))
-  .then(dispatch);
-
-
-const uploadProfileImageRequest = () => ({
-  type: constants.PROFILE_IMAGE_UPDATE_REQUEST,
-});
-
-
-const uploadProfileImageSuccess = () => ({
-  type: constants.PROFILE_IMAGE_UPDATE_SUCCESS,
-});
-
-
-const uploadProfileImageFailure = () => ({
-  type: constants.PROFILE_IMAGE_UPDATE_FAILURE,
-});
-
-
-const uploadProfileImage = (id, path) => (dispatch, { auth }) => Promise.resolve()
-  .then(() => uploadProfileImageRequest())
-  .then(dispatch)
-  .then(() => api.uploadProfileImage(id, path, auth))
-  .then(uploadProfileImageSuccess, trap(ApiResponseError, uploadProfileImageFailure))
-  .then(dispatch);
 
 
 export {
