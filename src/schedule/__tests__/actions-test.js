03b3b77f545d306480da0244861a92f044aa7c03
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-19 08:00:45 +0200

    Change approach for schedule action tests to checking that we call the action helpers with the expected params

--- src/schedule/__tests__/actions-test.js
+++ src/schedule/__tests__/actions-test.js
@@ -1,15 +1,18 @@
-jest.mock('src/api/schedule');
+jest.mock('src/actionHelpers', () => ({
+  apiAction: global.mock(),
+  staticAction: global.mock(),
+  dataAction: global.mock(),
+}));
 
-import { noop } from 'lodash';
+
 import * as constants from 'src/schedule/constants';
 import * as api from 'src/api';
 
 import {
-  capture,
-  fakeContext,
-  fakeScheduledCallData,
-  fakeScheduledCallListData,
-} from 'app/scripts/helpers';
+  apiAction,
+  staticAction,
+  dataAction,
+} from 'src/actionHelpers';
 
 import {
   listScheduledCalls,
@@ -24,239 +27,57 @@ const { ApiResponseError } = api;
 
 describe('schedule/actions', () => {
   describe('listScheduledCalls', () => {
-    beforeEach(() => {
-      api.listScheduledCalls.mockClear();
-
-      api.listScheduledCalls.mockReturnValue(
-        Promise.resolve(fakeScheduledCallListData()));
-    });
-
-    it('should dispatch request', async () => {
-      const [action] = await capture(listScheduledCalls(), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_LIST_REQUEST,
-      });
-    });
-
-    it('should dispatch success for successful listes', async () => {
-      const data = fakeScheduledCallListData();
-      api.listScheduledCalls.mockReturnValue(Promise.resolve(data));
-
-      const [_request, action] = await capture(listScheduledCalls(), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_LIST_SUCCESS,
-        payload: data,
-      });
-    });
-
-    it('should dispatch failure for failed listes', async () => {
-      api.listScheduledCalls.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
-
-      const [_request, action] = await capture(listScheduledCalls(), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_LIST_FAILURE,
-      });
-    });
-
-    it('should call api.listScheduledCalls() with the correct params', async () => {
-      const ctx = fakeContext();
-      const { auth } = ctx;
-
-      await listScheduledCalls()(noop, ctx);
-      expect(api.listScheduledCalls.mock.calls).toEqual([[auth]]);
+    it('should create actions for schedule api lists', () => {
+      expect(listScheduledCalls).toEqual(apiAction({
+        method: api.listScheduledCalls,
+        request: staticAction(constants.SCHEDULED_CALL_LIST_REQUEST),
+        success: dataAction(constants.SCHEDULED_CALL_LIST_SUCCESS),
+        failures: [[ApiResponseError, staticAction(constants.SCHEDULED_CALL_LIST_FAILURE)]],
+      }));
     });
   });
 
   describe('createScheduledCall', () => {
-    beforeEach(() => {
-      api.createScheduledCall.mockClear();
-      api.createScheduledCall.mockReturnValue(Promise.resolve(fakeScheduledCallData()));
-    });
-
-    it('should dispatch request', async () => {
-      const data = fakeScheduledCallData();
-      const [action] = await capture(createScheduledCall(23, data), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_CREATE_REQUEST,
-      });
-    });
-
-    it('should dispatch success for successful createes', async () => {
-      const data = fakeScheduledCallData();
-      api.createScheduledCall.mockReturnValue(Promise.resolve(data));
-
-      const [_request, action] = await capture(createScheduledCall(23, data), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_CREATE_SUCCESS,
-        payload: data,
-      });
-    });
-
-    it('should dispatch failure for failed createes', async () => {
-      api.createScheduledCall.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
-
-      const data = fakeScheduledCallData();
-      const [_request, action] = await capture(createScheduledCall(23, data), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_CREATE_FAILURE,
-      });
-    });
-
-    it('should call api.createScheduledCall() with the correct params', async () => {
-      const ctx = fakeContext();
-      const { auth } = ctx;
-      const data = fakeScheduledCallData();
-
-      await createScheduledCall(23, data)(noop, ctx);
-      expect(api.createScheduledCall.mock.calls).toEqual([[23, data, auth]]);
+    it('should create actions for schedule api creates', () => {
+      expect(createScheduledCall).toEqual(apiAction({
+        method: api.createScheduledCall,
+        request: staticAction(constants.SCHEDULED_CALL_CREATE_REQUEST),
+        success: dataAction(constants.SCHEDULED_CALL_CREATE_SUCCESS),
+        failures: [[ApiResponseError, staticAction(constants.SCHEDULED_CALL_CREATE_FAILURE)]],
+      }));
     });
   });
 
   describe('fetchScheduledCall', () => {
-    beforeEach(() => {
-      api.getScheduledCall.mockClear();
-      api.getScheduledCall.mockReturnValue(Promise.resolve(fakeScheduledCallData()));
-    });
-
-    it('should dispatch request', async () => {
-      const [action] = await capture(fetchScheduledCall(23), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_FETCH_REQUEST,
-      });
-    });
-
-    it('should dispatch success for successful fetches', async () => {
-      const data = fakeScheduledCallData();
-      api.getScheduledCall.mockReturnValue(Promise.resolve(data));
-
-      const [_request, action] = await capture(fetchScheduledCall(23), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_FETCH_SUCCESS,
-        payload: data,
-      });
-    });
-
-    it('should dispatch failure for failed fetches', async () => {
-      api.getScheduledCall.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
-
-      const [_request, action] = await capture(fetchScheduledCall(23), fakeContext());
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_FETCH_FAILURE,
-      });
-    });
-
-    it('should call api.getScheduledCall() with the correct params', async () => {
-      const ctx = fakeContext();
-      const { auth } = ctx;
-
-      await fetchScheduledCall(23)(noop, ctx);
-      expect(api.getScheduledCall.mock.calls).toEqual([[23, auth]]);
+    it('should create actions for schedule api fetches', () => {
+      expect(fetchScheduledCall).toEqual(apiAction({
+        method: api.getScheduledCall,
+        request: staticAction(constants.SCHEDULED_CALL_FETCH_REQUEST),
+        success: dataAction(constants.SCHEDULED_CALL_FETCH_SUCCESS),
+        failures: [[ApiResponseError, staticAction(constants.SCHEDULED_CALL_FETCH_FAILURE)]],
+      }));
     });
   });
 
   describe('updateScheduledCall', () => {
-    beforeEach(() => {
-      api.updateScheduledCall.mockClear();
-      api.updateScheduledCall.mockReturnValue(Promise.resolve(null));
-    });
-
-    it('should dispatch request', async () => {
-      const ctx = fakeContext();
-      const [action] = await capture(updateScheduledCall(23, fakeScheduledCallData()), ctx);
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_UPDATE_REQUEST,
-      });
-    });
-
-    it('should dispatch success for successful updates', async () => {
-      api.updateScheduledCall.mockReturnValue(Promise.resolve(null));
-
-      const ctx = fakeContext();
-      const [_request, action] = await capture(
-        updateScheduledCall(23, fakeScheduledCallData()), ctx);
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_UPDATE_SUCCESS,
-      });
-    });
-
-    it('should dispatch failure for failed updates', async () => {
-      api.updateScheduledCall.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
-
-      const ctx = fakeContext();
-      const [_request, action] = await capture(
-        updateScheduledCall(23, fakeScheduledCallData()), ctx);
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_UPDATE_FAILURE,
-      });
-    });
-
-    it('should call api.updateScheduledCall() with the correct params', async () => {
-      const ctx = fakeContext();
-      const { auth } = ctx;
-
-      await updateScheduledCall(23, fakeScheduledCallData())(noop, ctx);
-
-      expect(api.updateScheduledCall.mock.calls).toEqual([[
-        23, fakeScheduledCallData(), auth
-      ]]);
+    it('should create actions for schedule api updates', () => {
+      expect(updateScheduledCall).toEqual(apiAction({
+        method: api.updateScheduledCall,
+        request: staticAction(constants.SCHEDULED_CALL_UPDATE_REQUEST),
+        success: staticAction(constants.SCHEDULED_CALL_UPDATE_SUCCESS),
+        failures: [[ApiResponseError, staticAction(constants.SCHEDULED_CALL_UPDATE_FAILURE)]],
+      }));
     });
   });
 
   describe('removeScheduledCall', () => {
-    beforeEach(() => {
-      api.removeScheduledCall.mockClear();
-      api.removeScheduledCall.mockReturnValue(Promise.resolve(null));
-    });
-
-    it('should dispatch request', async () => {
-      const ctx = fakeContext();
-      const [action] = await capture(removeScheduledCall(23), ctx);
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_REMOVE_REQUEST,
-      });
-    });
-
-    it('should dispatch success for successful removes', async () => {
-      api.removeScheduledCall.mockReturnValue(Promise.resolve(null));
-
-      const ctx = fakeContext();
-      const [_request, action] = await capture(removeScheduledCall(23), ctx);
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_REMOVE_SUCCESS,
-      });
-    });
-
-    it('should dispatch failure for failed removes', async () => {
-      api.removeScheduledCall.mockReturnValue(Promise.reject(new ApiResponseError('o_O')));
-
-      const ctx = fakeContext();
-      const [_request, action] = await capture(removeScheduledCall(23), ctx);
-
-      expect(action).toEqual({
-        type: constants.SCHEDULED_CALL_REMOVE_FAILURE,
-      });
-    });
-
-    it('should call api.removeScheduledCall() with the correct params', async () => {
-      const ctx = fakeContext();
-      const { auth } = ctx;
-
-      await removeScheduledCall(23)(noop, ctx);
-      expect(api.removeScheduledCall.mock.calls).toEqual([[23, auth]]);
+    it('should create actions for schedule api removes', () => {
+      expect(removeScheduledCall).toEqual(apiAction({
+        method: api.removeScheduledCall,
+        request: staticAction(constants.SCHEDULED_CALL_REMOVE_REQUEST),
+        success: staticAction(constants.SCHEDULED_CALL_REMOVE_SUCCESS),
+        failures: [[ApiResponseError, staticAction(constants.SCHEDULED_CALL_REMOVE_FAILURE)]],
+      }));
     });
   });
 });
