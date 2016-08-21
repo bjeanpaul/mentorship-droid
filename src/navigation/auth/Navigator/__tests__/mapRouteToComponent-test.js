2959f6b8c8b67d5c6df4395bdd69f8b2c52cf6ac
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-21 23:48:32 +0200

    finished tests.

--- src/navigation/auth/Navigator/__tests__/mapRouteToComponent-test.js
+++ src/navigation/auth/Navigator/__tests__/mapRouteToComponent-test.js
@@ -1,17 +1,58 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+
 import mapRouteToComponent from '../mapRouteToComponent';
 import * as constants from 'src/navigation/auth/constants';
-
 import LandingContainer from 'src/onboarding/LandingContainer';
+import LoginContainer from 'src/auth/LoginContainer';
+import ActivationContainer from 'src/auth/ActivationContainer';
+import ResetPasswordContainer from 'src/auth/ResetPasswordContainer';
+
 
 // TODO: Login, Activation, ResetPassword
 describe('navigation/auth/navigator', () => {
-  describe('mapRouteToComponent', () => {
+  const mockStore = {
+    subscribe: () => {},
+    dispatch: () => {},
+    getState: () => {},
+  };
 
+  describe('mapRouteToComponent', () => {
     it('landing', () => {
-      expect(mapRouteToComponent(constants.AUTH_ROUTE_LANDING))
-        .toEqual(LandingContainer);
+      const el = shallow(
+        <Provider store={mockStore}>
+          {mapRouteToComponent(constants.AUTH_ROUTE_LANDING)}
+        </Provider>
+      );
+      expect(el.find(LandingContainer).length).toEqual(1);
     });
 
+    it('log in', () => {
+      const el = shallow(
+        <Provider store={mockStore}>
+          {mapRouteToComponent(constants.AUTH_ROUTE_LOGIN)}
+        </Provider>
+      );
+      expect(el.find(LoginContainer).length).toEqual(1);
+    });
 
+    it('activation', () => {
+      const el = shallow(
+        <Provider store={mockStore}>
+          {mapRouteToComponent(constants.AUTH_ROUTE_ACTIVATE)}
+        </Provider>
+      );
+      expect(el.find(ActivationContainer).length).toEqual(1);
+    });
+
+    it('reset password', () => {
+      const el = shallow(
+        <Provider store={mockStore}>
+          {mapRouteToComponent(constants.AUTH_ROUTE_RESET_PASSWORD)}
+        </Provider>
+      );
+      expect(el.find(ResetPasswordContainer).length).toEqual(1);
+    });
   });
 });
