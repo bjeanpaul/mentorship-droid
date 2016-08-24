4d23e4934ff940fd1a57a00b0b83a378659db58d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-24 15:14:52 +0200

    Don't use Title for category titles

--- src/categories/CategoryList/styles.js
+++ src/categories/CategoryList/styles.js
@@ -1,5 +1,8 @@
 import { StyleSheet } from 'react-native';
 
+import { FONT, FONT_WEIGHT } from 'src/constants/styles';
+import colors from 'src/constants/colors';
+
 
 export default StyleSheet.create({
   list: {
@@ -9,4 +12,10 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  categoryTitle: {
+    fontSize: 20,
+    fontFamily: FONT.MEDIUM,
+    fontWeight: FONT_WEIGHT.MEDIUM,
+    color: colors.TEXT_LIGHT,
+  }
 });
