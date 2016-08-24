f30c334e7b31af5e3066ff75d5004c6381b81b9d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-24 19:26:55 +0200

    Stub out CategoryAbout

---
+++ src/categories/CategoryAbout/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const CategoryList = () => (
+  <View>
+    {/* TODO */}
+  </View>
+);
+
+
+CategoryList.propTypes = {
+};
+
+
+export default CategoryList;
