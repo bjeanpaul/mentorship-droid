66f7adf5b5baa4a2b7927f227e1f9b22724af809
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-26 17:47:54 +0200

    updating tests.

--- src/components/Button/__tests__/Button-test.js
+++ src/components/Button/__tests__/Button-test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'src/components/Button';
+import Icon from 'src/components/Icon';
 
 
 describe('Button', () => {
@@ -7,60 +8,73 @@ describe('Button', () => {
 
   it('should render its `title,` and map `onPress`', () => {
     expect(render(
-      <Button
-        label="margle"
-        onPress={foo}
-      />
+      <Button onPress={foo}>
+        margle
+      </Button>
     )).toMatchSnapshot();
   });
 
   it('should render "light" theme', () => {
     expect(render(
       <Button
-        label="margle"
         onPress={foo}
         theme={Button.themes.light}
-      />
+      >
+        margle
+      </Button>
     )).toMatchSnapshot();
   });
 
   it('should render "transparent" theme', () => {
     expect(render(
       <Button
-        label="margle"
         onPress={foo}
         theme={Button.themes.transparent}
-      />
+      >
+        margle
+      </Button>
     )).toMatchSnapshot();
   });
 
   it('should render "inline" layout', () => {
     expect(render(
       <Button
-        label="margle"
         onPress={foo}
         layout={Button.layouts.inline}
-      />
+      >
+        margle
+      </Button>
     )).toMatchSnapshot();
   });
 
   it('should be disabled', () => {
     expect(render(
       <Button
-        label="margle"
         onPress={foo}
         disabled
-      />
+      >
+        margle
+      </Button>
+    )).toMatchSnapshot();
+  });
+
+  it('should take any kind of children', () => {
+    expect(render(
+      <Button
+        onPress={foo}
+      >
+        <Icon type={Icon.types.backOrange} />
+        I am text.
+      </Button>
     )).toMatchSnapshot();
   });
 
   it('should be able to tap and fire `onPress`', () => {
     const mockFn = jest.fn();
     const el = shallow(
-      <Button
-        label="margle"
-        onPress={mockFn}
-      />
+      <Button onPress={mockFn}>
+        margle
+      </Button>
     );
     el.simulate('press');
     expect(mockFn).toBeCalled();
