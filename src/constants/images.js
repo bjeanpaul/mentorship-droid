21c169585a2d03148947e2295a1a32490766434e
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-28 18:08:19 +0200

    Add activity section icon assets

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,6 +4,13 @@ import PROFILE_FACE_ADD from 'app/assets/Profile_Add.png';
 import BACK_LIGHT from 'app/assets/back-light.png';
 import ACTIVITY_ICON_FALLBACK from 'app/assets/activity-icon-fallback.png';
 import ACTIVITY_COMPLETE_ANNOTATION from 'app/assets/done.png';
+import ACTIVITY_INSTRUCTIONS from 'app/assets/instructions.png';
+import ACTIVITY_OBJECTIVE from 'app/assets/objective.png';
+import ACTIVITY_PROMPTS from 'app/assets/prompts.png';
+import ACTIVITY_RATIONALE from 'app/assets/rationale.png';
+import ACTIVITY_REFLECTION_POINTS from 'app/assets/reflection-points.png';
+import ACTIVITY_SKILLS_DEVELOPED from 'app/assets/skills-developed.png';
+import ACTIVITY_TOPIC from 'app/assets/topic.png';
 
 
 export default {
@@ -13,4 +20,11 @@ export default {
   BACK_LIGHT,
   ACTIVITY_ICON_FALLBACK,
   ACTIVITY_COMPLETE_ANNOTATION,
+  ACTIVITY_INSTRUCTIONS,
+  ACTIVITY_OBJECTIVE,
+  ACTIVITY_PROMPTS,
+  ACTIVITY_RATIONALE,
+  ACTIVITY_REFLECTION_POINTS,
+  ACTIVITY_SKILLS_DEVELOPED,
+  ACTIVITY_TOPIC,
 };
