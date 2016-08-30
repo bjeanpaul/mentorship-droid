d2a0f23e3f15d595b0c781f9c9a657afb1f99fd1
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-30 22:03:38 +0200

    Add loading view

---
+++ src/views/Loading/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { OverlayLoading } from 'src/components';
+
+
+const Loading = () => (
+  <OverlayLoading title="Loading your information..." />
+);
+
+
+export default Loading;
