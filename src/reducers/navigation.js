5a05f93a7ce51613f05bf0294e9491e990c48444
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-31 13:05:30 +0200

    adding routes.

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -7,7 +7,7 @@ import * as constants from 'src/constants/navigation';
 const navigation = (state = {
   index: 0,
   routes: [
-    { key: constants.ROUTE_LANDING },
+    { key: constants.ROUTE_ONBOARDING_SKILLS },
   ],
 }, action) => {
   switch (action.type) {
