56fd9a02a501318ccb733c8da442b4bb33c0b8ef
Peter Pistorius <peter.pistorius@gmail.com>
2016-08-31 10:51:55 +0200

    placeholders for following routes.

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -17,8 +17,12 @@ export const pushLoginRoute = makePushRoute(constants.ROUTE_AUTH_LOGIN);
 export const pushActivationRoute = makePushRoute(constants.ROUTE_AUTH_ACTIVATION);
 export const pushResetPasswordRoute = makePushRoute(constants.ROUTE_AUTH_RESET_PASSWORD);
 
+
 export const pushWelcomeRoute = makePushRoute(constants.ROUTE_ONBOARDING_WELCOME);
 export const pushProfilePictureRoute = makePushRoute(constants.ROUTE_ONBOARDING_PROFILE_PICTURE);
 export const pushCameraRollRoute = makePushRoute(constants.ROUTE_ONBOARDING_CAMERA_ROLL);
 export const pushOnboardingOccupationRoute = makePushRoute(constants.ROUTE_ONBOARDING_OCCUPATION);
-export const pushOnboardingMotivation = () => makePushRoute('TODO');
+export const pushOnboardingMotivation = makePushRoute(constants.ROUTE_ONBOARDING_MOTIVATION);
+export const pushOnboardingInspiration = makePushRoute(constants.ROUTE_ONBOARDING_INSPIRATION);
+export const pushOnboardingThreeWords = makePushRoute(constants.ROUTE_ONBOARDING_THREE_WORDS);
+export const pushOnboardingSkills = makePushRoute(constants.ROUTE_ONBOARDING_SKILLS);
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,16 @@ import * as constants from 'src/constants/navigation';
 import LandingContainer from 'src/containers/LandingContainer';
 import LoginContainer from 'src/containers/LoginContainer';
 import ActivationContainer from 'src/containers/ActivationContainer';
+
+
 import WelcomeContainer from 'src/containers/WelcomeContainer';
 import ProfilePictureContainer from 'src/containers/ProfilePictureContainer';
 import CameraRollContainer from 'src/containers/CameraRollContainer';
 import OnboardingOccupationContainer from 'src/containers/OnboardingOccupationContainer';
+import OnboardingMotivationContainer from 'src/containers/OnboardingMotivationContainer';
+//import OnboardingInspirationContainer from 'src/containers/OnboardingInspirationContainer';
+//import OnboardingThreeWordsContainer from 'src/containers/OnboardingThreeWordsContainer';
+//import OnboardingSkillsContainer from 'src/containers/OnboardingSkillsContainer';
 
 
 export default fromPairs([
@@ -20,4 +26,5 @@ export default fromPairs([
   [constants.ROUTE_ONBOARDING_PROFILE_PICTURE, ProfilePictureContainer],
   [constants.ROUTE_ONBOARDING_CAMERA_ROLL, CameraRollContainer],
   [constants.ROUTE_ONBOARDING_OCCUPATION, OnboardingOccupationContainer],
+  [constants.ROUTE_ONBOARDING_MOTIVATION, OnboardingMotivationContainer],
 ]);
