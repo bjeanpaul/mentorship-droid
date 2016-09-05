e2057ce1eddbf9d518703203550e221ad7ac8649
Peter Pistorius <peter.pistorius@gmail.com>
2016-09-05 15:15:18 +0200

    fixed profile picture tests.

--- src/views/ProfilePicture/__tests__/ProfilePicture-tests.js
+++ src/views/ProfilePicture/__tests__/ProfilePicture-tests.js
@@ -1,63 +1,43 @@
 import { noop } from 'lodash';
+import { Provider } from 'react-redux';
+import { fakeStore } from 'app/scripts/helpers';
 import React from 'react';
 import ProfilePicture from 'src/views/ProfilePicture';
 
-
 describe('ProfilePicture', () => {
   const createComponent = (props = {}) => (
-    <ProfilePicture
-      imagePath="path/to/image.png"
-      onNextPress={noop}
-      onBackPress={noop}
-      onChoosePhotoPress={noop}
-      {...props}
-    />
+    <Provider store={fakeStore}>
+      <ProfilePicture
+        profilePicture="path/to/image.png"
+        onChoosePhotoPress={noop}
+        {...props}
+      />
+    </Provider>
   );
 
   it('should map props correctly', () => {
     expect(render(createComponent())).toMatchSnapshot();
   });
 
-  it('should enable pagination next button if there is an imagePath', () => {
+  it('should enable pagination next button if there is an profilePicture', () => {
     expect(render(createComponent({
-      imagePath: 'path/to/image.png',
+      profilePicture: 'path/to/image.png',
     }))).toMatchSnapshot();
   });
 
-  it('should disable pagination next button if there is no imagePath', () => {
-    expect(render(createComponent({ imagePath: null }))).toMatchSnapshot();
+  it('should disable pagination next button if there is no profilePicture', () => {
+    expect(render(createComponent({ profilePicture: null }))).toMatchSnapshot();
   });
 
   it('should be able to tap and fire `onChoosePhotoPress`', () => {
     const onChoosePhotoPress = jest.fn();
-    const el = shallow(createComponent({ onChoosePhotoPress }));
-    el.find('TouchableWithoutFeedback').simulate('press');
-    expect(onChoosePhotoPress.mock.calls).toEqual([[]]);
-  });
-
-  it('should call onBackPress when back button is pressed', () => {
-    const onBackPress = jest.fn();
-    const el = shallow(createComponent({ onBackPress }));
-
-    el.find('Pagination')
-      .shallow()
-      .find('Button')
-      .at(0)
-      .simulate('press');
-
-    expect(onBackPress.mock.calls).toEqual([[]]);
-  });
-
-  it('should call onNextPress when next button is pressed', () => {
-    const onNextPress = jest.fn();
-    const el = shallow(createComponent({ onNextPress }));
-
-    el.find('Pagination')
-      .shallow()
-      .find('Button')
-      .at(1)
-      .simulate('press');
-
-    expect(onNextPress.mock.calls).toEqual([[]]);
+    const el = shallow(
+      <ProfilePicture
+        profilePicture="path/to/image.png"
+        onChoosePhotoPress={onChoosePhotoPress}
+      />
+    );
+    el.find('TouchableWithoutFeedback').at(0).simulate('press');
+    expect(onChoosePhotoPress).toBeCalled();
   });
 });
