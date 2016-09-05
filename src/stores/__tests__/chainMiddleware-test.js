bcb5a1e756bd46c6f697561025602f2ef730cfe5
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-05 09:24:00 +0200

    Fix chaining middleware to allow looping

--- src/stores/__tests__/chainMiddleware-test.js
+++ src/stores/__tests__/chainMiddleware-test.js
@@ -1,4 +1,3 @@
-import { noop } from 'lodash';
 import chainMiddleware from 'src/stores/chainMiddleware';
 import { capture } from 'app/scripts/helpers';
 
@@ -6,34 +5,40 @@ import { capture } from 'app/scripts/helpers';
 describe('chainMiddleware', () => {
   it('should chain actions with mappings', async () => {
     const next = jest.fn();
-    const target = () => dispatch => dispatch({ type: 'BAR' });
+    const dispatch = jest.fn();
+    const target = () => dispatchFn => dispatchFn({ type: 'BAR' });
+
     const middleware = chainMiddleware({ FOO: target });
+    await middleware({ dispatch })(next)({ type: 'FOO' });
 
-    await middleware({ dispatch: noop })(next)({ type: 'FOO' });
+    expect(next.mock.calls).toEqual([[{ type: 'FOO' }]]);
+    expect(dispatch.mock.calls.length).toEqual(1);
 
-    const [[a], [b]] = next.mock.calls;
-
-    expect(a).toEqual({ type: 'FOO' });
-    expect(await capture(b)).toEqual([{ type: 'BAR' }]);
+    const [[chained]] = dispatch.mock.calls;
+    expect(await capture(chained)).toEqual([{ type: 'BAR' }]);
   });
 
   it('should support non-thunks', async () => {
     const next = jest.fn();
+    const dispatch = jest.fn();
+
     const middleware = chainMiddleware({ FOO: () => ({ type: 'BAR' }) });
+    await middleware({ dispatch })(next)({ type: 'FOO' });
 
-    await middleware({ dispatch: noop })(next)({ type: 'FOO' });
+    expect(next.mock.calls).toEqual([[{ type: 'FOO' }]]);
+    expect(dispatch.mock.calls.length).toEqual(1);
 
-    const [[a], [b]] = next.mock.calls;
-
-    expect(a).toEqual({ type: 'FOO' });
-    expect(await capture(b)).toEqual([{ type: 'BAR' }]);
+    const [[chained]] = dispatch.mock.calls;
+    expect(await capture(chained)).toEqual([{ type: 'BAR' }]);
   });
 
   it('should pass on actions without mappings', async () => {
     const next = jest.fn();
+    const dispatch = jest.fn();
     const middleware = chainMiddleware({ FOO: () => ({ type: 'BAR' }) });
 
-    await middleware({ dispatch: noop })(next)({ type: 'BAZ' });
+    await middleware({ dispatch })(next)({ type: 'BAZ' });
     expect(next.mock.calls).toEqual([[{ type: 'BAZ' }]]);
+    expect(dispatch.mock.calls).toEqual([]);
   });
 });
