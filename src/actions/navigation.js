0e023fab6868a4f220ad78de7f60ddc5b7df7d52
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-06 16:54:42 +0200

    Add tab change nav action

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -5,3 +5,8 @@ import * as constants from 'src/constants/navigation';
 export const navigateBack = staticAction(constants.NAVIGATE_BACK_REQUEST);
 export const navigateForward = staticAction(constants.NAVIGATE_FORWARD_REQUEST);
 export const dismissScreen = staticAction(constants.SCREEN_DISMISS);
+
+export const changeNavTab = tab => ({
+  type: constants.NAV_TAB_CHANGE,
+  payload: { tab },
+});
