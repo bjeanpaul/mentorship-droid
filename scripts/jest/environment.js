1a646027be20927326440386e4d0bf06ed17bdbc
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-07 23:04:40 +0200

    No need to mock out NavigationTransitioner

--- scripts/jest/environment.js
+++ scripts/jest/environment.js
@@ -11,7 +11,6 @@ global.render = render;
 // github.com/facebook/jest/issues/1384
 jest.mock('TextInput', () => 'TextInput');
 jest.mock('ScrollView', () => 'ScrollView');
-jest.mock('NavigationTransitioner', () => 'NavigationTransitioner');
 
 // automocking doesn't seem to work for axios
 jest.setMock('axios', jest.fn());
