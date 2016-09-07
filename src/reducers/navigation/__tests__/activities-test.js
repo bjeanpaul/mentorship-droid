98b712c0c77ea105a20e845d0eedeeb71a3e03e6
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-07 22:53:56 +0200

    Add new navigation reducers that work with new three tier navigation

---
+++ src/reducers/navigation/__tests__/activities-test.js
@@ -0,0 +1,34 @@
+import reduce from 'src/reducers/navigation/activities';
+import * as activities from 'src/actions/activities';
+import * as routes from 'src/constants/routes';
+import { createStack, createRoute, push } from 'src/navigationHelpers';
+
+
+describe('src/reducers/navigation/activities', () => {
+  describe('CATEGORY_CHOOSE', () => {
+    it('should push on the category route', () => {
+      expect(reduce(createStack(), activities.chooseCategory(23)))
+        .toEqual(push(createStack(), createRoute(routes.ROUTE_CATEGORY, {
+          categoryId: 23,
+        })));
+    });
+  });
+
+  describe('ACTIVITY_CHOOSE', () => {
+    it('should push on the activity route', () => {
+      expect(reduce(createStack(), activities.chooseActivity(23)))
+        .toEqual(push(createStack(), createRoute(routes.ROUTE_ACTIVITY, {
+          activityId: 23,
+        })));
+    });
+  });
+
+  describe('ACTIVITY_SCHEDULE_CALL', () => {
+    it('should push on the activity route', () => {
+      expect(reduce(createStack(), activities.scheduleActivityCall(23)))
+        .toEqual(push(createStack(), createRoute(routes.ROUTE_SCHEDULE_CALL, {
+          activityId: 23,
+        })));
+    });
+  });
+});
