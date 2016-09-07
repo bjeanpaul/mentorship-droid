e2818221b24596c499c2b4f66be6875ed17a1044
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-07 22:57:19 +0200

    rm dead nav code

--- src/containers/NavigationStackContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-
-import NavigationStack from 'src/views/NavigationStack';
-import { getCurrentStack } from 'src/navigationHelpers';
-
-
-export default connect(
-  ({ routes }) => ({ navigationState: getCurrentStack(routes) })
-)(NavigationStack);
