47f98a8d6d6dc1b7181d52d8e089d0c74c410c8d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-09 12:07:16 +0200

    Add loading failure view

---
+++ src/views/LoadingFailure/__tests__/LoadingFailure-test.js
@@ -0,0 +1,39 @@
+import { noop } from 'lodash';
+import React from 'react';
+
+import LoadingFailure from 'src/views/LoadingFailure';
+import { uidEquals } from 'app/scripts/helpers';
+
+
+describe('LoadingFailure', () => {
+  const createComponent = (props = {}) => (
+    <LoadingFailure
+      onDismissPress={noop}
+      {...props}
+    />
+  );
+
+  it('should render', () => {
+    expect(render(createComponent())).toMatchSnapshot();
+  });
+
+  it('should call onDismissPress when the back button is pressed', () => {
+    const onDismissPress = jest.fn();
+    const el = shallow(createComponent({ onDismissPress }));
+
+    el.findWhere(uidEquals('back'))
+      .simulate('press');
+
+    expect(onDismissPress.mock.calls).toEqual([[]]);
+  });
+
+  it('should call onDismissPress when the dismiss button is pressed', () => {
+    const onDismissPress = jest.fn();
+    const el = shallow(createComponent({ onDismissPress }));
+
+    el.findWhere(uidEquals('dismiss'))
+      .simulate('press');
+
+    expect(onDismissPress.mock.calls).toEqual([[]]);
+  });
+});
