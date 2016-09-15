fba0c0ea9ea422f04bbc46c3be3f6c58b2ba0e08
Peter Pistorius <peter.pistorius@gmail.com>
2016-09-15 11:56:01 +0200

    add action for fetching list of events

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,6 @@ export * from 'src/api/profiles';
 export * from 'src/api/schedule';
 export * from 'src/api/activities';
 export * from 'src/api/categories';
+export * from 'src/api/events';
 export * from 'src/api/errors';
 export * from 'src/api/sync';
