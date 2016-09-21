249454ae5d117153246e61738fb2b9601b0bf9a0
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-21 18:27:32 +0200

    Update StartCallContainer to handle cases where scheduled call has no activity

--- src/containers/__tests__/StartCallContainer-test.js
+++ src/containers/__tests__/StartCallContainer-test.js
@@ -1,5 +1,11 @@
-import { mapStateToProps } from 'src/containers/StartCallContainer';
+jest.mock('src/actions/calls', () => ({
+  createCall: global.mock(),
+}));
+
+import { mapStateToProps, mapDispatchToProps } from 'src/containers/StartCallContainer';
 import { fakeState, fakeScheduledCall, fakeActivity } from 'app/scripts/helpers';
+import { createCall } from 'src/actions/calls';
+import { dismissScreen } from 'src/actions/navigation';
 
 
 describe('StartCallContainer', () => {
@@ -48,6 +54,39 @@ describe('StartCallContainer', () => {
 
     it('should not provide anything if a scheduled call id isnt given', () => {
       expect(mapStateToProps(fakeState(), {})).toEqual({});
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should map onActivatePress to createCall()', () => {
+      const dispatch = jest.fn();
+      const { onActivatePress } = mapDispatchToProps(dispatch);
+
+      expect(dispatch.mock.calls).toEqual([]);
+
+      onActivatePress(fakeActivity());
+
+      expect(dispatch.mock.calls).toEqual([[
+        createCall({ activity: fakeActivity() }),
+      ]]);
+    });
+
+    it('should not pass an activity to createCall() if there is no relevant activity', () => {
+      const dispatch = jest.fn();
+      const { onActivatePress } = mapDispatchToProps(dispatch);
+
+      expect(dispatch.mock.calls).toEqual([]);
+      onActivatePress(void 0);
+      expect(dispatch.mock.calls).toEqual([[createCall({})]]);
+    });
+
+    it('should map onDismissPress to dismissScreen()', () => {
+      const dispatch = jest.fn();
+      const { onDismissPress } = mapDispatchToProps(dispatch, { callId: 23 });
+
+      expect(dispatch.mock.calls).toEqual([]);
+      onDismissPress();
+      expect(dispatch.mock.calls).toEqual([[dismissScreen()]]);
     });
   });
 });
