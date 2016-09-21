a0986ce787bb4c6ef707182ce40c737188c4f85c
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-21 20:48:48 +0200

    Add scheduled calls to synced data

--- src/api/sync.js
+++ src/api/sync.js
@@ -1,12 +1,14 @@
 import { merge, map } from 'lodash';
 import { listCategories } from 'src/api/categories';
 import { listActivities } from 'src/api/activities';
+import { listScheduledCalls } from 'src/api/schedule';
 
 
 export const load = auth => Promise.resolve()
   .then(() => Promise.all([
     listCategories(auth),
     listActivities(auth),
+    listScheduledCalls(auth),
   ]))
   .then((res) => merge(...map(res, 'entities')))
   .then(entities => ({ entities }));
