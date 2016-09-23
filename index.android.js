9eb204b1c9e6a7a60435f2281678da573eb8c0f3
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-23 15:06:46 +0200

    Revert accidental change to index.android.js

--- index.android.js
+++ index.android.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import ScheduleList from 'src/views/ScheduleList';
+import MentorshipApp from 'src/app';
 import { AppRegistry } from 'react-native';
 
-AppRegistry.registerComponent('app', () => () => (
-  <ScheduleList
-    onCallChosen={() => null}
-    calls={[{
-      callTime: '2016-09-21T19:40:09.880Z',
-    }, {
-      callTime: '2016-09-23T19:40:09.880Z',
-      activity: {
-        title: 'Activity 23',
-      },
-    }]}
-  />
-));
+AppRegistry.registerComponent('app', () => () => <MentorshipApp />);
