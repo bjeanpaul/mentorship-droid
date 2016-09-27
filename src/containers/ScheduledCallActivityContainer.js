7361afb27735f0769244b7ef59b00965fc526da0
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-27 15:03:23 +0200

    Update sched call activity container to provide category data

--- src/containers/ScheduledCallActivityContainer.js
+++ src/containers/ScheduledCallActivityContainer.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import ActivityList from 'src/views/ActivityList';
-import { getCategoryActivities } from 'src/stores/helpers';
+import { getCategory, getCategoryActivities } from 'src/stores/helpers';
 import { chooseScheduledCallActivity } from 'src/actions/schedule';
 import { dismissScreen } from 'src/actions/navigation';
 
 
 export const mapStateToProps = (state, { categoryId }) => ({
+  category: getCategory(state, categoryId),
   activities: getCategoryActivities(state, categoryId),
 });
 
--- src/containers/__tests__/ScheduledCallActivityContainer-test.js
+++ src/containers/__tests__/ScheduledCallActivityContainer-test.js
@@ -4,6 +4,22 @@ import { fakeState, fakeActivity, fakeCategory } from 'app/scripts/helpers';
 
 describe('ScheduledCallActivityContainer', () => {
   describe('mapStateToProps', () => {
+    it('should provide the category', () => {
+      const category = fakeCategory({ id: 21 });
+
+      expect(mapStateToProps(fakeState({
+        entities: {
+          categories: {
+            21: category,
+          },
+        },
+      }), {
+        categoryId: 21,
+      })).toEqual(jasmine.objectContaining({
+        category,
+      }));
+    });
+
     it('should provide all activities of the relevant category', () => {
       const activity1 = fakeActivity({
         id: 1,
