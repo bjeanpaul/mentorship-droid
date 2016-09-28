de4fcff81cee8b8df19e0bf75cc3c68c512ada70
Peter Pistorius <peter.pistorius@gmail.com>
2016-09-28 14:40:38 +0200

    added container and list view

---
+++ src/containers/CallNoteListContainer.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import CallNoteList from 'src/views/CallNoteList';
+
+// TODO
+export const mapStateToProps = state => ({
+})
+
+export default connect()(CallNoteList);
---
+++ src/views/CallNoteList/index.js
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react';
+import { ScrollView } from 'react-native';
+import { BaseView } from 'src/components';
+import styles from './styles';
+
+
+const CallNoteList = ({
+  callNotes = [],
+}) => (
+  <BaseView>
+    <ScrollView>
+    </ScrollView>
+  </BaseView>
+);
+
+
+CallNoteList.propTypes = {
+  callNotes: PropTypes.array.isRequired,
+};
+
+
+export default CallNoteList;
