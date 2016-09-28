02e106c6797a7223e74a129a0f42a2c886e1d70d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-09-28 17:59:46 +0200

    Map back buttons to dismissScreen() action creator in login and activation containers

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 import { login } from 'src/actions/auth';
+import { dismissScreen } from 'src/actions/navigation';
 import Login from 'src/views/Login';
 
 
-export default connect(
-  (state) => state.auth, {
-    onLoginPress: login,
-  }
-)(Login);
+export default connect(state => state.auth, {
+  onLoginPress: login,
+  onBackPress: dismissScreen,
+})(Login);
