7683cdc476642dff901be184b80aae4256a7b749
Peter Pistorius <peter.pistorius@gmail.com>
2016-09-30 19:21:37 +0200

    display next scheduled call date

--- src/containers/JourneyContainer.js
+++ src/containers/JourneyContainer.js
@@ -3,10 +3,11 @@ import { openCall } from 'src/actions/journey';
 import Journey from 'src/views/Journey';
 import noop from 'lodash';
 import { changeNavToScheduledCallsTab } from 'src/actions/navigation';
+import { getNextScheduledCall } from 'src/stores/helpers';
 
 
-export default connect(() => ({
-  nextScheduledCallDate: void 0, // TODO
+export default connect(state => ({
+  nextScheduledCallDate: getNextScheduledCall(state),
 }), {
   onNextScheduledCallPress: changeNavToScheduledCallsTab,
   onCallPress: openCall,
