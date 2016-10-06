df4d9e0428c115a155e834389198e19497712636
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-06 21:46:05 +0200

    Add routes to new error views

--- src/actions/errors.js
+++ src/actions/errors.js
@@ -5,5 +5,5 @@ import { ApiResponseError, NetworkError } from 'src/api';
 
 export default [
   [ApiResponseError, staticAction(constants.NETWORK_ERROR)],
-  [NetworkError, staticAction(constants.API_RESPONSE_ERROR)],
+  [NetworkError, staticAction(constants.API_ERROR)],
 ];
