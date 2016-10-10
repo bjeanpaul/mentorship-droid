131cef5006987be819d961a28b87c499934225bb
Peter Pistorius <peter.pistorius@gmail.com>
2016-10-10 14:01:36 +0200

    removing patch

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { AsyncStorage } from 'react-native';
 
 import { getContext } from 'src/stores/helpers';
 import contextMiddleware from 'src/stores/contextMiddleware';
@@ -10,13 +9,8 @@ import effects from 'src/effects';
 
 
 export default function configureStore(initialState = {}) {
-  const reduce = (state, action) => (
-    action.type === 'hydrate'
-      ? action.payload.state
-      : rootReducer(state, action));
-
-  const store = createStore(
-    reduce,
+  return createStore(
+    rootReducer,
     initialState,
     applyMiddleware(
       chainMiddleware(effects),
@@ -24,34 +18,4 @@ export default function configureStore(initialState = {}) {
       thunkMiddleware,
     )
   );
-
-  store.subscribe(() => {
-    AsyncStorage.setItem('debug:mentorship', JSON.stringify(store.getState()));
-  });
-
-  // AsyncStorage.setItem('debug:mentorship', '');
-
-  AsyncStorage.getItem('debug:mentorship')
-    .then(v => {
-      if (v) {
-        const state = JSON.parse(v);
-
-        // (state.navigation.top = {
-        //   index: 0,
-        //   routes: [
-        //     {
-        //       key: 'ROUTE_CREATE_CALL_NOTES',
-        //     },
-        //   ],
-        // });
-        store.dispatch({
-          type: 'hydrate',
-          payload: {
-            state,
-          },
-        });
-      }
-    });
-
-  return store;
 }
