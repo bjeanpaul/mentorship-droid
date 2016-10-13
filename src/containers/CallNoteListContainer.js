dd126fb59837a1d37f1daa8b0457dea8d1086590
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-13 20:54:27 +0200

    Map call note list container props to the relevant action creators

--- src/containers/CallNoteListContainer.js
+++ src/containers/CallNoteListContainer.js
@@ -1,17 +1,19 @@
 import { connect } from 'react-redux';
 import CallNoteList from 'src/views/CallNoteList';
 import { getActivityCallNotes } from 'src/stores/helpers';
-import { noop } from 'lodash';
+import { dismissScreen } from 'src/actions/navigation';
+import { chooseCallNote } from 'src/actions/callNote';
 
 
+// TODO handle call notes without activities once we have designs for this
 export const mapStateToProps = (state, { activityId }) => ({
   callNotes: getActivityCallNotes(state, activityId),
 });
 
 
 export const mapDispatchToProps = {
-  onRowPress: () => noop, // TODO
-  onDismissPress: () => noop, // TODO
+  onRowPress: chooseCallNote,
+  onDismissPress: dismissScreen,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CallNoteList);
