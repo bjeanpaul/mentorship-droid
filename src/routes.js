a55909281c7a70250ff0444ade9ef0fbbb3a5334
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-13 20:30:57 +0200

    Add routes to call notes list and detail containers

--- src/routes.js
+++ src/routes.js
@@ -38,6 +38,8 @@ import CallCompletedContainer from 'src/containers/CallCompletedContainer';
 
 import CallNoteStepperContainer from 'src/containers/CallNoteStepperContainer';
 import CallNoteSavedContainer from 'src/containers/CallNoteSavedContainer';
+import CallNoteListContainer from 'src/containers/CallNoteListContainer';
+import CallNoteDetailContainer from 'src/containers/CallNoteDetailContainer';
 
 
 export default fromPairs([
@@ -77,6 +79,7 @@ export default fromPairs([
   [constants.ROUTE_CALL_COMPLETED, CallCompletedContainer],
 
   [constants.ROUTE_CREATE_CALL_NOTES, CallNoteStepperContainer],
-
   [constants.ROUTE_CALL_NOTE_SAVED, CallNoteSavedContainer],
+  [constants.ROUTE_CALL_NOTE_LIST, CallNoteListContainer],
+  [constants.ROUTE_CALL_NOTE_DETAIL, CallNoteDetailContainer],
 ]);
