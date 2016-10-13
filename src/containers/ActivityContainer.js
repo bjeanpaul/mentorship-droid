56aff91b3b4c75da88a7d77c01f5e101f9c141d0
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-13 20:48:56 +0200

    Update activity container to provide the next scheduled call and latest call notes

--- src/containers/ActivityContainer.js
+++ src/containers/ActivityContainer.js
@@ -1,25 +1,45 @@
+import { last } from 'lodash';
 import { connect } from 'react-redux';
 import Activity from 'src/views/Activity';
-import { getCategory, getActivity } from 'src/stores/helpers';
-import { scheduleActivityCall, dismissActivityScreen } from 'src/actions/activities';
+
+import { openScheduledCall } from 'src/actions/schedule';
+
+import {
+  getCategory,
+  getActivity,
+  getNextScheduledCall,
+  getActivityCallNotes,
+} from 'src/stores/helpers';
+
+import {
+  scheduleActivityCall,
+  viewActivityCallNotes,
+  dismissActivityScreen,
+} from 'src/actions/activities';
 
 
 const mapStateToProps = (state, { activityId }) => {
   const activity = getActivity(state, activityId);
   const { category: categoryId } = activity;
   const category = getCategory(state, categoryId);
+  const nextScheduledCall = getNextScheduledCall(state, { activity: activityId });
+  const latestCallNotes = last(getActivityCallNotes(state, activityId));
 
   return {
     category,
     activity,
+    nextScheduledCall,
+    latestCallNotes,
   };
 };
 
 
 const propsToActions = {
   // TODO
-  onSchedulePress: scheduleActivityCall,
   onBackPress: dismissActivityScreen,
+  onSchedulePress: scheduleActivityCall,
+  onReschedulePress: openScheduledCall,
+  onViewCallNotesPress: viewActivityCallNotes,
 };
 
 
