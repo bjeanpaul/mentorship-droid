ac1e241c22addc41c25e2ea5e0b4ee44b72bc1ab
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-19 21:34:19 +0200

    Update CallNoteSavedContainer to match recent view changes

--- src/containers/CallNoteSavedContainer.js
+++ src/containers/CallNoteSavedContainer.js
@@ -1,8 +1,28 @@
 import { connect } from 'react-redux';
 import CallNoteSaved from 'src/views/CallNoteSaved';
+import { addNextScheduledCall } from 'src/actions/schedule';
 import { dismissScreen } from 'src/actions/navigation';
+import { getCallNote, getActivity } from 'src/stores/helpers';
+
+
+const mapStateToProps = (state, { callNoteId }) => {
+  const callNote = getCallNote(state, callNoteId);
+  const { callActivity } = callNote;
+  const activity = callActivity && getActivity(state, callActivity);
+
+  return {
+    callNote,
+    activity,
+  };
+};
+
+
+export {
+  mapStateToProps,
+};
 
 
 export default connect(null, {
   onDismissPress: dismissScreen,
+  onScheduleNextPress: addNextScheduledCall,
 })(CallNoteSaved);
