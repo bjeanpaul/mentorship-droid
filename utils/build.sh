a5d5410d8c4bee87c55e6834186398f6296cf7e0
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-25 15:59:04 +0200

    Provide build script with ENVFILE var

--- utils/build.sh
+++ utils/build.sh
@@ -2,6 +2,6 @@
 set -e
 
 cd android
-./gradlew assembleRelease -PdisablePreDex
+ENVFILE=$ENVFILE ./gradlew assembleRelease -PdisablePreDex
 cd ..
 mv android/app/build/outputs/apk/app-release.apk $1
