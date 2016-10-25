3ee40e61b4828a9bdfd7f239958ce0d7e5852d12
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-25 17:57:40 +0200

    Support tappable event components

--- src/components/Event/__tests__/Event-tests.js
+++ src/components/Event/__tests__/Event-tests.js
@@ -1,27 +1,49 @@
 import React from 'react';
+import { noop } from 'lodash';
+
 import { IMAGE } from 'app/scripts/fixtures';
 import Event from 'src/components/Event';
 
 
 describe('Event', () => {
+  const createComponent = (props = {}) => (
+    <Event
+      date="2016-09-16T11:19:17.368442Z"
+      icon={IMAGE}
+      title="pew pew pew"
+      blurb="i am a blurb"
+      {...props}
+    />
+  );
+
   it('should render the props', () => {
-    expect(render(
-      <Event
-        date={'2016-09-16T11:19:17.368442Z'}
-        icon={IMAGE}
-        title="pew pew pew"
-        blurb="i am a blurb"
-      />
-    )).toMatchSnapshot();
+    const el = render(createComponent());
+    expect(el.toJSON()).toMatchSnapshot();
   });
 
   it('should render a formatted date if the blurb is undefined', () => {
-    expect(render(
-      <Event
-        date={'2016-09-16T11:19:17.368442Z'}
-        icon={IMAGE}
-        title="pew pew pew"
-      />
-    )).toMatchSnapshot();
+    const el = render(createComponent({
+      date: '2016-09-16T11:19:17.368442Z',
+      blurb: void 0,
+    }));
+
+    expect(el.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with a "more" indicator if onPress is given', () => {
+    const el = render(createComponent({
+      onPress: noop,
+    }));
+
+    expect(el.toJSON()).toMatchSnapshot();
+  });
+
+  it('should call onPress when pressed', () => {
+    const onPress = jest.fn();
+
+    shallow(createComponent({ onPress }))
+      .simulate('press');
+
+    expect(onPress.mock.calls).toEqual([[]]);
   });
 });
--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,27 +1,41 @@
 import moment from 'moment';
 import React, { PropTypes } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, TouchableNativeFeedback } from 'react-native';
 import { Text } from 'src/components';
-import styles from './styles';
+import styles from 'src/components/Event/styles';
+import images from 'src/constants/images';
 
 const Event = ({
   date,
   icon,
   title,
   blurb,
+  onPress,
 }) => (
-  <View style={styles.container}>
-    <View style={styles.icon}>
-      <Image style={styles.iconImage} source={icon} />
-    </View>
-    <View style={styles.textContainer}>
-      <Text style={styles.title}>{title}</Text>
+  <TouchableNativeFeedback onPress={() => onPress && onPress()}>
+    <View style={styles.container}>
+      <View style={styles.icon}>
+        <Image style={styles.iconImage} source={icon} />
+      </View>
 
-      <Text numberOfLines={1} style={styles.blurb}>
-        {blurb || moment(date).format('dddd Do, MMMM YYYY')}
-      </Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+
+        <Text numberOfLines={1} style={styles.blurb}>
+          {blurb || moment(date).format('dddd Do, MMMM YYYY')}
+        </Text>
+      </View>
+
+      {
+        onPress && <View style={styles.moreContainer}>
+          <Image
+            style={styles.more}
+            source={images.EVENT_MORE}
+          />
+        </View>
+      }
     </View>
-  </View>
+  </TouchableNativeFeedback>
 );
 
 
@@ -30,6 +44,7 @@ Event.propTypes = {
   icon: PropTypes.any.isRequired,
   title: PropTypes.string.isRequired,
   blurb: PropTypes.string,
+  onPress: PropTypes.func,
 };
 
 
