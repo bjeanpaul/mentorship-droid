66f3e49b2df5db88a7ac2591aa5fc11204599a78
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-26 15:54:16 +0200

    Fix calendar view to work on smaller screens

--- src/views/ScheduleListCalendar/styles.js
+++ src/views/ScheduleListCalendar/styles.js
@@ -4,20 +4,46 @@ import colors from 'src/constants/colors';
 import { FONT, FONT_WEIGHT } from 'src/constants/styles';
 
 
+const calendarFontSize = 14;
+
+
 export default StyleSheet.create({
   calendarContainer: {
     flex: 1,
   },
   calendarControls: {
+    alignItems: 'center',
     backgroundColor: colors.CALENDAR_CONTROLS_BG,
+    paddingVertical: 7,
+    paddingHorizontal: 23,
   },
   controlButtonText: {
+    margin: 0,
     fontFamily: FONT.REGULAR,
     fontSize: 24,
     color: colors.CALENDAR_CONTROLS_TEXT,
   },
+  calendarHeading: {
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    backgroundColor: colors.CALENDAR_HEADING,
+    paddingBottom: 7,
+  },
+  dayHeading: {
+    fontFamily: FONT.REGULAR,
+    fontSize: calendarFontSize,
+    color: colors.CALENDAR_DAY_HEADING,
+    marginVertical: 0,
+  },
+  weekendHeading: {
+    fontFamily: FONT.REGULAR,
+    fontSize: calendarFontSize,
+    color: colors.CALENDAR_WEEKEND_HEADING,
+    marginVertical: 0,
+  },
   title: {
-    paddingTop: 4,
+    margin: 0,
+    marginTop: 4,
     fontFamily: FONT.MEDIUM,
     fontWeight: FONT_WEIGHT.MEDIUM,
     fontSize: 20,
@@ -25,18 +51,22 @@ export default StyleSheet.create({
     color: colors.CALENDAR_TITLE,
   },
   dayButton: {
+    padding: 0,
     borderTopWidth: 0,
     flex: 1,
     justifyContent: 'center',
   },
+  dayButtonFiller: {
+    padding: 0,
+  },
   day: {
     fontFamily: FONT.REGULAR,
-    fontSize: 15,
+    fontSize: calendarFontSize,
     color: colors.CALENDAR_WEEKEND_DAY_TEXT,
   },
   weekendDayText: {
     fontFamily: FONT.REGULAR,
-    fontSize: 15,
+    fontSize: calendarFontSize,
     color: colors.CALENDAR_WEEKEND_DAY_TEXT,
   },
   monthContainer: {
@@ -46,33 +76,26 @@ export default StyleSheet.create({
   weekRow: {
     flex: 1,
   },
-  calendarHeading: {
-    height: 44,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    backgroundColor: colors.CALENDAR_HEADING,
-  },
-  dayHeading: { // day of the week, above the calendar
-    fontFamily: FONT.REGULAR,
-    fontSize: 15,
-    color: colors.CALENDAR_DAY_HEADING,
-  },
-  weekendHeading: { // day of the week, weekend baby
-    fontFamily: FONT.REGULAR,
-    fontSize: 15,
-    color: colors.CALENDAR_WEEKEND_HEADING,
-  },
   eventIndicator: {
     backgroundColor: colors.CALENDAR_EVENT_BG,
-    height: 10,
-    width: 10,
-    borderRadius: 10,
+  },
+  eventIndicatorFiller: {
+    width: 6,
+    height: 6,
+    borderRadius: 6,
+    marginTop: -6,
   },
   currentDayText: {
     color: colors.CALENDAR_CURRENT_DAY_TEXT,
   },
   currentDayCircle: {
     backgroundColor: colors.CALENDAR_CURRENT_DAY_BG,
+  },
+  dayCircleFiller: {
+    width: 30,
+    height: 30,
+    borderRadius: 30,
+    zIndex: 9,
   },
   selectedDayCircle: {
     backgroundColor: colors.CALENDAR_SELECTED_DAY_BG,
