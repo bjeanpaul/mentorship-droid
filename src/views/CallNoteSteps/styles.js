016d07b3a3815a73089190ca4447833ee4cf77d6
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-28 13:59:06 +0200

    Fix call note mood images to align properly

--- src/views/CallNoteSteps/styles.js
+++ src/views/CallNoteSteps/styles.js
@@ -1,9 +1,13 @@
 import { merge } from 'lodash';
 import { StyleSheet } from 'react-native';
 
-import { DEVICE_HEIGHT, DEVICE_HEIGHT_SMALL } from 'src/constants/styles';
+import { DEVICE_WIDTH, DEVICE_HEIGHT, DEVICE_HEIGHT_SMALL } from 'src/constants/styles';
 import colors from 'src/constants/colors';
 
+const SMALL_CIRCLE_SIZE = 66;
+const LARGE_CIRCLE_SIZE = 77;
+
+// TODO class names that suck less
 
 export default StyleSheet.create(merge({
   list: {
@@ -13,24 +17,25 @@ export default StyleSheet.create(merge({
     alignItems: 'center',
   },
   item: {
-    margin: 10,
+    marginVertical: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    width: (DEVICE_WIDTH / 3) - 5,
   },
   image: {
-    width: 66,
-    height: 66,
+    width: SMALL_CIRCLE_SIZE,
+    height: SMALL_CIRCLE_SIZE,
+    borderRadius: SMALL_CIRCLE_SIZE,
   },
   imageHighlight: {
     backgroundColor: colors.CALL_NOTE_MOOD_IMAGE_DARKEN,
-    width: 66,
-    height: 66,
-    borderRadius: 66,
+    width: SMALL_CIRCLE_SIZE,
+    height: SMALL_CIRCLE_SIZE,
+    borderRadius: SMALL_CIRCLE_SIZE,
   },
   imageIsSelected: {
     borderWidth: 4,
     borderColor: colors.CALL_NOTE_MOOD_IMAGE_SELECTED_BORDER,
-    backgroundColor: 'transparent',
   },
   label: {
     fontSize: 18,
@@ -64,12 +69,13 @@ export default StyleSheet.create(merge({
   },
 }, DEVICE_HEIGHT > DEVICE_HEIGHT_SMALL && {
   image: {
-    width: 77,
-    height: 77,
+    width: LARGE_CIRCLE_SIZE,
+    height: LARGE_CIRCLE_SIZE,
+    borderRadius: LARGE_CIRCLE_SIZE,
   },
   imageHighlight: {
-    width: 77,
-    height: 77,
-    borderRadius: 77,
+    width: LARGE_CIRCLE_SIZE,
+    height: LARGE_CIRCLE_SIZE,
+    borderRadius: LARGE_CIRCLE_SIZE,
   },
 }));
