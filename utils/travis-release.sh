513c54724633e74a3fc711b20393c91a31f6b46e
justinvdm <justinvdm.0.1.0@gmail.com>
2016-11-02 16:12:05 +0200

    Change build scripts to support separate qa and prd google-services.json files

--- utils/travis-release.sh
+++ utils/travis-release.sh
@@ -2,11 +2,20 @@
 set -e
 
 release() {
-  ENVFILE=$ENVFILE ./utils/build.sh $APK
+  GOOGLE_SERVICES=$GOOGLE_SERVICES ENVFILE=$ENVFILE ./utils/build.sh $APK
   ./utils/bundle.sh
   TAG=$TRAVIS_TAG SENTRY_URL=$SENTRY_URL SENTRY_TOKEN=$SENTRY_TOKEN ./utils/sentry-upload-artefacts.sh
 }
 
-APK=mentorship-$TRAVIS_TAG.apk ENVFILE=.env.prd SENTRY_URL=$SENTRY_PRD_URL SENTRY_TOKEN=$SENTRY_PRD_TOKEN release
+APK=mentorship-$TRAVIS_TAG.apk \
+  ENVFILE=.env.prd \
+  GOOGLE_SERVICES=google-services.prd.json \
+  SENTRY_URL=$SENTRY_PRD_URL \
+  SENTRY_TOKEN=$SENTRY_PRD_TOKEN release
 
-APK=mentorship-$TRAVIS_TAG.qa.apk ENVFILE=.env.qa SENTRY_URL=$SENTRY_QA_URL SENTRY_TOKEN=$SENTRY_QA_TOKEN release
+APK=mentorship-$TRAVIS_TAG.qa.apk \
+  ENVFILE=.env.qa \
+  GOOGLE_SERVICES=google-services.qa.json \
+  SENTRY_URL=$SENTRY_QA_URL \
+  SENTRY_TOKEN=$SENTRY_QA_TOKEN \
+  release
