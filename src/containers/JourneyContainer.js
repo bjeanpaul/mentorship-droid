178e035900caaa90d041d55ae3bedd40515ac49e
justinvdm <justinvdm.0.1.0@gmail.com>
2016-11-03 16:25:20 +0200

    Get eslint to ignore our intentional error condition stub thing

--- src/containers/JourneyContainer.js
+++ src/containers/JourneyContainer.js
@@ -9,7 +9,8 @@ import { openCall } from 'src/actions/calls';
 
 
 const mapStateToProps = state => {
-  throw new Error('Testing sentry');
+  const foo = true;
+  if (foo) throw new Error('Testing sentry');
   const [start, end] = [START_CALL_RANGE_START, START_CALL_RANGE_END];
   const scheduledCall = first(getScheduledCallsBetween(state, start, end));
 
