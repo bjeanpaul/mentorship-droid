6f2ac00e22074ee7c7719d3141093906325b519d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-11-22 18:23:35 +0200

    Remove dead onboarding views and containers

--- src/containers/OnboardingFormStepContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { FormStep } from 'src/components';
-import * as actions from 'src/actions/onboarding';
-
-
-export default connect(null, {
-  onBackPress: actions.stepBack,
-  onNextPress: actions.stepForward,
-})(FormStep);
