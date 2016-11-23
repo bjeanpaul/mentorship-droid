dd6e9ec3e0c6fbe63563327d0c2a902e4626fe0a
justinvdm <justinvdm.0.1.0@gmail.com>
2016-11-23 11:35:13 +0200

    Update Pagination to handle the last page better

--- src/components/Pagination/__tests__/Pagination-test.js
+++ src/components/Pagination/__tests__/Pagination-test.js
@@ -1,61 +1,79 @@
+import { noop } from 'lodash';
 import React from 'react';
 import Pagination from 'src/components/Pagination';
+import { uidEquals } from 'app/scripts/helpers';
 
 
 describe('Button', () => {
-  function foo() {}
-  function bar() {}
+  const createComponent = (props = {}) => (
+      <Pagination
+        onBackPress={noop}
+        onNextPress={noop}
+        onDonePress={noop}
+        {...props}
+      />
+  );
 
   it('should render props correctly', () => {
-    expect(render(
-      <Pagination
-        onBackPress={foo}
-        onNextPress={bar}
-      />
-    )).toMatchSnapshot();
+    const el = render(createComponent());
+    expect(el.toJSON()).toMatchSnapshot();
   });
 
   it('should support disabling its back button', () => {
-    expect(render(
-      <Pagination
-        onBackPress={foo}
-        onNextPress={bar}
-        backDisabled
-      />
-    )).toMatchSnapshot();
+    const el = render(createComponent({ backDisabled: true }));
+    expect(el.toJSON()).toMatchSnapshot();
   });
 
-  it('should be disabled', () => {
-    expect(render(
-      <Pagination
-        onBackPress={foo}
-        onNextPress={bar}
-        disabled
-      />
-    )).toMatchSnapshot();
+  it('should disable its next button if disabled is true', () => {
+    const el = render(createComponent({ disabled: true }));
+    expect(el.toJSON()).toMatchSnapshot();
   });
 
-  it('should be able to tap and fire `onNextPress`', () => {
-    const mockFn = jest.fn();
-    const el = shallow(
-      <Pagination
-        onBackPress={foo}
-        onNextPress={mockFn}
-      />
-    );
-    el.find('Button').at(1).simulate('press');
-    expect(mockFn).toBeCalled();
+  it('should render a done button if last is true', () => {
+    const el = render(createComponent({ last: true }));
+    expect(el.toJSON()).toMatchSnapshot();
   });
 
-  it('should be able to tap and fire `onBackPress`', () => {
-    const mockFn = jest.fn();
-    const el = shallow(
-      <Pagination
-        onBackPress={mockFn}
-        onNextPress={bar}
-      />
-    );
-    el.find('Button').at(0).simulate('press');
-    expect(mockFn).toBeCalled();
+  it('should disable its done button if disabled is true', () => {
+    const el = render(createComponent({
+      last: true,
+      disabled: true,
+    }));
+
+    expect(el.toJSON()).toMatchSnapshot();
+  });
+
+  it('should call onBackPress when back is pressed', () => {
+    const onBackPress = jest.fn();
+    const el = shallow(createComponent({ onBackPress }));
+
+    el.findWhere(uidEquals('back'))
+      .simulate('press');
+
+    expect(onBackPress).toBeCalled();
+  });
+
+  it('should call onNextPress when next is pressed', () => {
+    const onNextPress = jest.fn();
+    const el = shallow(createComponent({ onNextPress }));
+
+    el.findWhere(uidEquals('next'))
+      .simulate('press');
+
+    expect(onNextPress).toBeCalled();
+  });
+
+  it('should call onDonePress when done is pressed', () => {
+    const onDonePress = jest.fn();
+
+    const el = shallow(createComponent({
+      last: true,
+      onDonePress
+    }));
+
+    el.findWhere(uidEquals('done'))
+      .simulate('press');
+
+    expect(onDonePress).toBeCalled();
   });
 });
