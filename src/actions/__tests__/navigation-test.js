f952b811a1c44ed87338cc085a6caa584f4c6bf0
justinvdm <justinvdm.0.1.0@gmail.com>
2016-11-28 21:56:11 +0200

    Clean up changeNavTab() action

--- src/actions/__tests__/navigation-test.js
+++ src/actions/__tests__/navigation-test.js
@@ -6,7 +6,8 @@ describe('actions/navigation', () => {
   describe('changeNavTab', () => {
     it('should create an action for changing the nav tab', () => {
       expect(actions.changeNavTab(constants.NAV_TAB_ACTIVITIES)).toEqual({
-        type: constants.NAV_TAB_ACTIVITIES,
+        type: constants.NAV_TAB_CHANGE,
+        payload: { tab: constants.NAV_TAB_ACTIVITIES },
       });
     });
   });
--- src/reducers/navigation/__tests__/navigator-test.js
+++ src/reducers/navigation/__tests__/navigator-test.js
@@ -4,30 +4,10 @@ import * as constants from 'src/constants/navigation';
 
 
 describe('src/reducers/navigation/navigator', () => {
-  describe('should switch the active tab for the action.type', () => {
+  describe('should switch the active tab to tab given by the action', () => {
     it('NAV_TAB_CHANGE_ACTIVITIES', () => {
       expect(reduce(void 0, actions.changeNavTab(constants.NAV_TAB_ACTIVITIES)))
       .toEqual({ activeTab: constants.NAV_TAB_ACTIVITIES });
-    });
-
-    it('NAV_TAB_CHANGE_CHAT', () => {
-      expect(reduce(void 0, actions.changeNavTab(constants.NAV_TAB_CHAT)))
-      .toEqual({ activeTab: constants.NAV_TAB_CHAT });
-    });
-
-    it('NAV_TAB_CHANGE_COMMUNITY', () => {
-      expect(reduce(void 0, actions.changeNavTab(constants.NAV_TAB_COMMUNITY)))
-      .toEqual({ activeTab: constants.NAV_TAB_COMMUNITY });
-    });
-
-    it('NAV_TAB_CHANGE_JOURNEY', () => {
-      expect(reduce(void 0, actions.changeNavTab(constants.NAV_TAB_JOURNEY)))
-      .toEqual({ activeTab: constants.NAV_TAB_JOURNEY });
-    });
-
-    it('NAV_TAB_CHANGE_SCHEDULED_CALLS', () => {
-      expect(reduce(void 0, actions.changeNavTab(constants.NAV_TAB_SCHEDULED_CALLS)))
-      .toEqual({ activeTab: constants.NAV_TAB_SCHEDULED_CALLS });
     });
   });
 });
