905236007cb7a1da739b5900dad6c4c9f27cc4d7
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-01 21:45:03 +0200

    Provide messages in messages container

--- src/containers/MessagesContainer.js
+++ src/containers/MessagesContainer.js
@@ -1,5 +1,14 @@
 import { connect } from 'react-redux';
 import Messages from 'src/views/Messages';
+import { getMessages } from 'src/store/helpers';
 
 
-export default connect()(Messages);
+// TODO group messages
+export const mapStateToProps = state => ({
+  groups: [{
+    messages: getMessages(state),
+  }],
+});
+
+
+export default connect(mapStateToProps)(Messages);
