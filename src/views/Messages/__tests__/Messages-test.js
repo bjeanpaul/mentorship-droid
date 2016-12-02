cdf1ff07595ac28a80292de67d2bbd5fca75bd10
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-02 15:55:23 +0200

    Update Messages view to include send message input

--- src/views/Messages/__tests__/Messages-test.js
+++ src/views/Messages/__tests__/Messages-test.js
@@ -1,7 +1,8 @@
+import { noop } from 'lodash';
 import React from 'react';
 
 import Messages from 'src/views/Messages';
-import { fakeMessage } from 'app/scripts/helpers';
+import { fakeMessage, uidEquals } from 'app/scripts/helpers';
 
 
 describe('Messages', () => {
@@ -27,6 +28,7 @@ describe('Messages', () => {
             }),
           ],
         }]}
+        onSendPress={noop}
         {...props}
       />
     );
@@ -35,5 +37,21 @@ describe('Messages', () => {
   it('should render', () => {
     const el = render(createComponent());
     expect(el.toJSON()).toMatchSnapshot();
+  });
+
+  it('should call onSendPress() with input content when pressed', () => {
+    const onSendPress = jest.fn();
+    const el = shallow(createComponent({ onSendPress }));
+
+    const send = el.find('Send')
+      .shallow();
+
+    send.findWhere(uidEquals('sendInput'))
+      .simulate('textChange', '123');
+
+    send.findWhere(uidEquals('sendButton'))
+      .simulate('press');
+
+    expect(onSendPress.mock.calls).toEqual([[{ content: '123' }]]);
   });
 });
--- src/views/Messages/styles.js
+++ src/views/Messages/styles.js
@@ -1,18 +1,61 @@
 import { StyleSheet } from 'react-native';
+import { FONT } from 'src/constants/styles';
 import colors from 'src/constants/colors';
 
 
 export default StyleSheet.create({
-  messageContent: {
-    fontSize: 16,
+  messages: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  message: {
+    marginVertical: 24,
+  },
+  bubble: {
+    flex: 1,
     paddingTop: 14,
-    paddingBottom: 18,
+    paddingBottom: 17,
     paddingHorizontal: 24,
     borderRadius: 6,
     backgroundColor: colors.BG_DEFAULT,
   },
+  bubbleSent: {
+    backgroundColor: colors.MESSAGE_SENT_BG,
+  },
+  messageContent: {
+    fontSize: 16,
+    textAlign: 'left',
+  },
   messageContentSent: {
     color: colors.TEXT_LIGHT,
-    backgroundColor: colors.MESSAGE_RECEIVED_BG,
+  },
+  send: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 16,
+    borderTopColor: colors.SEND_BORDER,
+    borderTopWidth: 1,
+  },
+  bubbleSend: {
+    // there seems to be some vertical padding on TextInput that can't be removed
+    paddingTop: 10,
+    paddingBottom: 12,
+  },
+  sendInputText: {
+    borderBottomWidth: 0,
+    fontFamily: FONT.REGULAR,
+    fontSize: 16,
+    color: colors.TEXT_INPUT_TEXT,
+    padding: 0,
+  },
+  sendButton: {
+    marginLeft: 12,
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+    backgroundColor: colors.BUTTON_BG,
+  },
+  sendButtonImage: {
+    padding: 8,
   },
 });
