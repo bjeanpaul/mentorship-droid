2e87af70c32d37e519c9c4003b6d9698824f9ace
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-05 17:52:20 +0200

    Fix message api tests after change to message result parsing

--- src/api/__tests__/messages-test.js
+++ src/api/__tests__/messages-test.js
@@ -2,7 +2,7 @@ jest.mock('src/api/request');
 
 import { identity } from 'lodash';
 import { normalize, arrayOf } from 'normalizr';
-import { fakeAuth, fakeMessage } from 'app/scripts/helpers';
+import { fakeAuth } from 'app/scripts/helpers';
 import request from 'src/api/request';
 import { Message, PendingMessage } from 'src/api/schemas';
 import { parseResults, parseSendMessageResult } from 'src/api/parse';
@@ -42,20 +42,18 @@ describe('api/messages', () => {
         content: 'Set tall in your just beyond merch booth bound chest',
       });
 
-      const { parse, ...res } = sendMessage(msg, fakeAuth());
+      const res = sendMessage(msg, fakeAuth());
 
       expect(res).toEqual({
         url: '/chat_message/',
         method: 'POST',
         schema: arrayOf(Message),
         auth: fakeAuth(),
+        parse: parseSendMessageResult,
         data: {
           content: 'Set tall in your just beyond merch booth bound chest',
         },
       });
-
-      expect(parse(fakeMessage()))
-        .toEqual(parseSendMessageResult(msg)(fakeMessage()));
     });
   });
 
