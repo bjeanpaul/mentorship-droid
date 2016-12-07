ba6ff57ff13c818a0e8ec08169b01db94172d109
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-07 12:01:03 +0200

    Remove unused styles from Messages styles module

--- src/views/Messages/styles.js
+++ src/views/Messages/styles.js
@@ -1,17 +1,4 @@
 import { StyleSheet } from 'react-native';
-import colors from 'src/constants/colors';
-
-
-export const bubbleThemes = {
-  dark: StyleSheet.create({
-    bubble: {
-      backgroundColor: colors.MESSAGE_SENT_BG,
-    },
-    bubbleContent: {
-      color: colors.TEXT_LIGHT,
-    },
-  }),
-};
 
 
 export default StyleSheet.create({
