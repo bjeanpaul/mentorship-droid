c25c7a5e0d608d9aa339acd585e490e0aa7964fb
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-13 13:41:24 +0200

    Fix effects module to start and listen for message poll ticks correctly

--- src/effects.js
+++ src/effects.js
@@ -6,16 +6,22 @@ import { enter } from 'src/actions/entry';
 import { load } from 'src/actions/sync';
 import { sequence } from 'src/actionHelpers';
 import { setupNotifications } from 'src/actions/notifications';
-import { listRecentMessages } from 'src/actions/messages';
+import { startMessagePolling, listRecentMessages } from 'src/actions/messages';
 import { EVENT_POLL_TICK } from 'src/constants/events';
-import { NEW_MESSAGE_RECEIVED, MESSAGE_POLL_TICK } from 'src/constants/notifications';
+import { MESSAGE_POLL_TICK } from 'src/constants/messages';
+import { NEW_MESSAGE_RECEIVED } from 'src/constants/notifications';
 
 import { SCHEDULED_CALL_CREATE_SUCCESS } from 'src/constants/schedule';
 import { listEvents, startEventPolling } from 'src/actions/events';
 
 
 export default fromPairs([
-  [AUTH_LOGIN_SUCCESS, sequence([enter, setupNotifications, startEventPolling])],
+  [AUTH_LOGIN_SUCCESS, sequence([
+    enter,
+    setupNotifications,
+    startEventPolling,
+    startMessagePolling,
+  ])],
   [EXISTING_USER_ENTER, load],
   [ONBOARDING_SETUP_PROFILE_SUCCESS, load],
   [SCHEDULED_CALL_CREATE_SUCCESS, listEvents],
