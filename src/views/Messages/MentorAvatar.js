326d37aa1abbcd1f285c7a8d5eabbf1b500b21f3
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-15 17:55:11 +0200

    Add resize params to profile image url for mentor's message avatar

--- src/views/Messages/MentorAvatar.js
+++ src/views/Messages/MentorAvatar.js
@@ -1,18 +1,19 @@
 import React, { PropTypes } from 'react';
 
+import { ImageUrl } from 'src/api';
 import Avatar from './Avatar';
 
 
 const MentorAvatar = ({
   profile: { profilePic },
 }) => (
-  <Avatar source={{ uri: profilePic }} />
+  <Avatar source={profilePic.resize(40, 40).toSource()} />
 );
 
 
 MentorAvatar.propTypes = {
   profile: PropTypes.shape({
-    profilePic: PropTypes.string.isRequired,
+    profilePic: PropTypes.instanceOf(ImageUrl).isRequired,
   }).isRequired,
 };
 
