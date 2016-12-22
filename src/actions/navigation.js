d4ead7196e6f8d4b1aca1b0f5a9d9587553ea714
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-22 21:32:21 +0200

    Add an action creator for native dismiss actions

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -1,5 +1,10 @@
 import { staticAction } from 'src/actionHelpers';
 import * as constants from 'src/constants/navigation';
+import BackAndroid from 'BackAndroid';
+
+import * as routes from 'src/constants/routes';
+import { getActiveTopRoute } from 'src/store/helpers';
+import { logout } from 'src/actions/auth';
 
 
 export const dismissScreen = staticAction(constants.SCREEN_DISMISS);
@@ -9,3 +14,20 @@ export const changeNavTab = tab => ({
   type: constants.NAV_TAB_CHANGE,
   payload: { tab },
 });
+
+
+export const dismissNative = () => (dispatch, ctx, getState) => {
+  const state = getState();
+  const activeRoute = getActiveTopRoute(state);
+
+  switch (activeRoute.key) {
+    case routes.ROUTE_LANDING:
+      return BackAndroid.exitApp();
+
+    case routes.ROUTE_NAVIGATOR:
+      return dispatch(logout());
+
+    default:
+      return dispatch(dismissScreen());
+  }
+};
