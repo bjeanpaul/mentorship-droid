df690e2cafe1931854e19f4605651dc18f3e7561
justinvdm <justinvdm.0.1.0@gmail.com>
2016-12-23 12:41:43 +0200

    Mark activities as complete if objective achieved for created call notes in entities reducer

--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -1,7 +1,8 @@
-import { includes } from 'lodash';
-import merge from 'lodash/merge';
+import { includes, fromPairs } from 'lodash';
+import { merge } from 'lodash/fp';
 import { AUTH_LOGOUT } from 'src/constants/auth';
 import { ACTIONS_WITH_ENTITIES } from 'src/constants/entities';
+import { CALL_NOTE_CREATE_SUCCESS } from 'src/constants/callNotes';
 
 
 export const createInitialState = () => ({
@@ -15,7 +16,7 @@ export const createInitialState = () => ({
 
 const mergeActionEntities = (state, action) => {
   return includes(ACTIONS_WITH_ENTITIES, action.type)
-    ? merge({}, state, action.payload.entities)
+    ? merge(state, action.payload.entities)
     : state;
 };
 
@@ -24,6 +25,24 @@ const entitiesReducer = (state, action) => {
   switch (action.type) {
     case AUTH_LOGOUT:
       return createInitialState();
+
+    case CALL_NOTE_CREATE_SUCCESS: {
+      const { payload } = action;
+      console.log(payload);
+
+      const {
+        callActivity,
+        objectiveAchieved,
+      } = payload.entities.callNotes[payload.result];
+
+      return callActivity && objectiveAchieved
+        ? merge(state, {
+          activities: fromPairs([
+            [callActivity, { isComplete: true }],
+          ]),
+        })
+        : state;
+    }
 
     default:
       return state;
