6ec2f1dba20ab6f81c1b3085d742d78f2f90840d
justinvdm <justinvdm.0.1.0@gmail.com>
2017-01-05 16:11:56 +0200

    Style RichText text

--- src/components/RichText/styles.js
+++ src/components/RichText/styles.js
@@ -1,9 +1,34 @@
-import { StyleSheet } from 'react-native';
+import { omit } from 'lodash';
+import mdStyles from 'react-native-simple-markdown/styles';
+
+import colors from 'src/constants/colors';
+import Text from 'src/components/Text';
 
 
-export default StyleSheet.create({
+export default {
   container: {
+    paddingHorizontal: 16,
   },
   del: {
   },
-});
+  listItemNumber: {
+    color: colors.TEXT_DEFAULT,
+  },
+  listItemBullet: {
+    color: colors.TEXT_DEFAULT,
+  },
+  blockQuoteSectionBar: {
+    ...mdStyles.blockQuoteSectionBar,
+    backgroundColor: colors.BG_DARK,
+  },
+  text: Text.styles.default,
+  u: {
+  },
+  listItemText: omit(mdStyles.listItemText, 'color'),
+  table: {
+  },
+  tableHeader: {
+  },
+  tableRow: {
+  },
+};
