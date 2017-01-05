8d3f1d8a847b854352ab75113107a5b4c4b3406e
justinvdm <justinvdm.0.1.0@gmail.com>
2017-01-05 13:11:17 +0200

    Get basics working for RichText

--- src/components/RichText/__tests__/RichText-test.js
+++ src/components/RichText/__tests__/RichText-test.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import { RichText } from 'src/components';
+import { richText } from 'src/api';
 
 
 describe('RichText', () => {
-  it('should render text');
+  it('should render typography', () => {
+    // TODO get underlining working
+    // TODO get inline code working
+    // TODO get code blocks working
+    expect(render(
+      <RichText>{richText()(`
+        # h1
+
+        ## h2
+
+        ### h3
+
+        #### h4
+
+        ##### h5
+
+        ###### h6
+
+        text
+        [link](http://www.google.com)
+
+        *italics*
+        **bold**
+        ***bold italics***
+        ~~strikeout~~
+
+        > block quote
+
+        - unordered
+        - list
+
+        1. numbered
+        2. list
+      `)}</RichText>
+    )).toMatchSnapshot();
+  });
 });
--- src/components/RichText/index.js
+++ src/components/RichText/index.js
@@ -1,8 +1,12 @@
+import { merge } from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { View } from 'react-native';
+import SimpleMarkdown from 'simple-markdown';
 import mdStyles from 'react-native-simple-markdown/styles';
+import mdRules from 'react-native-simple-markdown/rules';
 
 import { RichText as RichTextObject } from 'src/api';
+import baseRules from './rules';
 import baseStyles from './styles';
 
 
@@ -17,11 +21,27 @@ class RichText extends Component {
     };
   }
 
+  getRules(styles) {
+    return merge(
+      SimpleMarkdown.defaultRules,
+      mdRules(styles),
+      baseRules(styles));
+  }
+
+  getRenderFn(styles) {
+    const rules = this.getRules(styles);
+    return SimpleMarkdown.reactFor(SimpleMarkdown.ruleOutput(rules, 'react'));
+  }
+
   render() {
     const styles = this.getStyles();
+    const renderFn = this.getRenderFn(styles);
+    const { children } = this.props;
 
     return (
-      <View style={styles.container} />
+      <View style={styles.container}>
+        {renderFn(children.tree)}
+      </View>
     );
   }
 }
