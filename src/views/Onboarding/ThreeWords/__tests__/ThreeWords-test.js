5649b0513e79f2c17904f2e2bd564f33934cd484
justinvdm <justinvdm.0.1.0@gmail.com>
2017-01-09 14:34:36 +0200

    More robust word parsing for Three Words onboarding screen

--- src/views/Onboarding/ThreeWords/__tests__/ThreeWords-test.js
+++ src/views/Onboarding/ThreeWords/__tests__/ThreeWords-test.js
@@ -1,7 +1,7 @@
 import { noop } from 'lodash';
 import React from 'react';
 
-import { fakeProfile } from 'app/scripts/helpers';
+import { uidEquals, fakeProfile } from 'app/scripts/helpers';
 import { ThreeWords } from 'src/views/Onboarding';
 
 
@@ -19,6 +19,19 @@ describe('ThreeWords', () => {
   it('should render', () => {
     const el = render(createComponent());
     expect(el.toJSON()).toMatchSnapshot();
+  });
+
+  it('should disable pagination if more or less than 3 words have been given', () => {
+    const paginationDisabled = tags =>
+      shallow(createComponent({ profile: fakeProfile({ tags }) }))
+        .findWhere(uidEquals('formStep'))
+        .prop('paginationDisabled');
+
+    expect(paginationDisabled('')).toBe(true);
+    expect(paginationDisabled('foo bar')).toBe(true);
+    expect(paginationDisabled('foo bar baz quux')).toBe(true);
+    expect(paginationDisabled('foo bar baz')).toBe(false);
+    expect(paginationDisabled('foo,bar  baz')).toBe(false);
   });
 });
 
