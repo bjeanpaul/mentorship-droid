a7f4e5b3ce46bc4fb1c478e3f4bb983742e4bae7
justinvdm <justinvdm.0.1.0@gmail.com>
2017-02-17 17:56:48 +0200

    Provide blog posts in BlogPostListContainer

--- src/containers/BlogPostListContainer.js
+++ src/containers/BlogPostListContainer.js
@@ -1,8 +1,12 @@
 import { connect } from 'react-redux';
+
+import { getBlogPosts } from 'src/store/helpers';
 import BlogPostList from 'src/views/BlogPostList';
 
 
-export const mapStateToProps = () => ({
+export const mapStateToProps = state => ({
+  blogPosts: getBlogPosts(state)
+    .filter(({ isHidden }) => !isHidden),
 });
 
 
--- src/containers/__tests__/BlogPostListContainer-test.js
+++ src/containers/__tests__/BlogPostListContainer-test.js
@@ -1,5 +1,37 @@
+import { fakeState, fakeBlogPost } from 'app/scripts/helpers';
+import { mapStateToProps } from 'src/containers/BlogPostListContainer';
+
+
 describe('BlogPostListContainer', () => {
   describe('mapStateToProps', () => {
-    xit('should provide all visible blog posts');
+    it('should provide all visible blog posts', () => {
+      const state = fakeState();
+
+      const blogPost1 = fakeBlogPost({
+        id: 1,
+      });
+
+      const blogPost2 = fakeBlogPost({
+        id: 2,
+        isHidden: true,
+      });
+
+      const blogPost3 = fakeBlogPost({
+        id: 3,
+      });
+
+      state.entities = {
+        blogPosts: {
+          1: blogPost1,
+          2: blogPost2,
+          3: blogPost3,
+        },
+      };
+
+      expect(mapStateToProps(state))
+        .toEqual(jasmine.objectContaining({
+          blogPosts: [blogPost1, blogPost3],
+        }));
+    });
   });
 });
