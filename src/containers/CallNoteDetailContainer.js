6a2376cfa7d2a92768bf14343187a051cd495b29
nathanbegbie <nathanbegbie@gmail.com>
2017-05-05 08:53:29 +0200

    Update CallNoteDetailContainer to handle both versions of call notes
    And pass call to CallNoteDetail View to delegate which view maps to which version

--- src/containers/CallNoteDetailContainer.js
+++ src/containers/CallNoteDetailContainer.js
@@ -1,14 +1,16 @@
 import { connect } from 'react-redux';
 import CallNoteDetail from 'src/views/CallNoteDetail';
-import { getCallNote, getActivity } from 'src/store/helpers';
+import { getCallNote, getCall, getActivity } from 'src/store/helpers';
 import { dismissScreen } from 'src/actions/navigation';
 
 const mapStateToProps = (state, { callNoteId }) => {
   const callNote = getCallNote(state, callNoteId);
+  const call = getCall(state, callNote.call);
   const activity = callNote.callActivity && getActivity(state, callNote.callActivity);
 
   return {
     callNote,
+    call,
     activity,
   };
 };
--- src/containers/__tests__/CallNoteDetailContainer-test.js
+++ src/containers/__tests__/CallNoteDetailContainer-test.js
@@ -1,38 +1,103 @@
 import { mapStateToProps } from 'src/containers/CallNoteDetailContainer';
-import { fakeState, fakeCallNote, fakeActivity } from 'app/scripts/helpers';
+import {
+  fakeState,
+  fakeCallNote,
+  fakeCallNoteV2,
+  fakeCall,
+  fakeActivity,
+} from 'app/scripts/helpers';
 
 
 describe('CallNoteDetailContainer', () => {
   describe('mapStateToProps', () => {
-    it('should provide the call note', () => {
-      const callNote = fakeCallNote({ id: 21 });
+    const call = fakeCall({ id: 15 });
+    let callNote = fakeCallNote({
+      id: 21,
+      call: 15,
+    });
+    const activity = fakeActivity({
+      id: 50,
+      objective: 'Let us make a Snowflake',
+      icon: 'http://icon.of.snowflake.com',
+    });
 
+    it('should provide the version 1 call note', () => {
       const state = fakeState({
         entities: {
           callNotes: {
             21: callNote,
           },
+          calls: {
+            15: call,
+          },
         },
       });
 
       expect(mapStateToProps(state, { callNoteId: 21 }))
-        .toEqual(jasmine.objectContaining({ callNote }));
+        .toEqual(jasmine.objectContaining({
+          callNote,
+          call,
+        }));
     });
 
-    it('should provide the activity', () => {
-      const activity = fakeActivity({
-        id: 50,
-        objective: 'Let us make a Snowflake',
-        icon: 'http://icon.of.snowflake.com',
+    it('should provide the version 2 call note', () => {
+      callNote = fakeCallNoteV2({
+        id: 21,
+        call: 15,
+      });
+      const state = fakeState({
+        entities: {
+          callNotes: {
+            21: callNote,
+          },
+          calls: {
+            15: call,
+          },
+        },
+      });
+
+      expect(mapStateToProps(state, { callNoteId: 21 }))
+        .toEqual(jasmine.objectContaining({
+          callNote,
+          call,
+        }));
+    });
+
+    it('should provide the activity with call note version 1', () => {
+      callNote.callActivity = 50;
+
+      const state = fakeState({
+        entities: {
+          calls: {
+            15: call,
+          },
+          callNotes: {
+            21: callNote,
+          },
+          activities: {
+            50: activity,
+          },
+        },
+      });
+
+      expect(mapStateToProps(state, { callNoteId: 21 }))
+        .toEqual(jasmine.objectContaining({ activity }));
+    });
+
+    it('should provide the activity with call note version 2', () => {
+      callNote = fakeCallNoteV2({
+        id: 21,
+        call: 15,
+        callActivity: 50,
       });
 
       const state = fakeState({
         entities: {
+          calls: {
+            15: call,
+          },
           callNotes: {
-            21: fakeCallNote({
-              id: 21,
-              callActivity: 50,
-            }),
+            21: callNote,
           },
           activities: {
             50: activity,
