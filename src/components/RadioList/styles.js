aec01147ac4b1535e712f2f9db0d29e981c38ccd
justinvdm <justinvdm.0.1.0@gmail.com>
2017-05-10 14:22:32 +0200

    Update RadioList styles to handle longer items better

--- src/components/RadioList/styles.js
+++ src/components/RadioList/styles.js
@@ -1,7 +1,10 @@
+import { mergeAll } from 'lodash/fp';
 import { StyleSheet } from 'react-native';
-import colors from 'src/constants/colors';
 
-export default StyleSheet.create({
+import colors from 'src/constants/colors';
+import { DEVICE_WIDTH, DEVICE_WIDTH_MEDIUM } from 'src/constants/styles';
+
+export default StyleSheet.create(mergeAll([{
   container: {
     flex: 1,
     marginLeft: 40,
@@ -14,13 +17,18 @@ export default StyleSheet.create({
     borderColor: colors.RADIO_LIST_BORDER,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
   },
   itemLabel: {
     color: colors.RADIO_LIST_TEXT,
-    fontSize: 18,
+    fontSize: 14,
   },
   itemImage: {
     height: 32,
     width: 32,
   },
-});
+}, DEVICE_WIDTH > DEVICE_WIDTH_MEDIUM && {
+  itemLabel: {
+    fontSize: 16,
+  },
+}]));
