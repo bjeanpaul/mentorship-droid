15549dc46a34ec439a83aab59997c82ad86fce41
justinvdm <justinvdm.0.1.0@gmail.com>
2017-05-15 21:55:44 +0200

    Remove .component use in Radio tests

--- src/components/Radio/__tests__/Radio-tests.js
+++ src/components/Radio/__tests__/Radio-tests.js
@@ -5,10 +5,10 @@ import { Radio, RadioItem } from 'src/components';
 describe('Radio', () => {
   it('should render', () => {
     expect(render(
-      <Radio.component>
-        <RadioItem.component value="a">A</RadioItem.component>
-        <RadioItem.component value="b">B</RadioItem.component>
-      </Radio.component>
+      <Radio>
+        <RadioItem value="a">A</RadioItem>
+        <RadioItem value="b">B</RadioItem>
+      </Radio>
     )).toMatchSnapshot();
   });
 });
