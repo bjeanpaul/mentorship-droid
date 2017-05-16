bb815a5859a0240ff5e9af9a7384f91e64f3156c
nathanbegbie <nathanbegbie@gmail.com>
2017-05-16 11:37:57 +0200

    Add 'forgot password' link to login screen

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import { login } from 'src/actions/auth';
+import { showForgotPasswordEmail } from 'src/actions/forgotPassword';
 import { dismissScreen } from 'src/actions/navigation';
 import Login from 'src/views/Login';
 
 
 export default connect(state => state.auth, {
+  onForgotPasswordPress: showForgotPasswordEmail,
   onLoginPress: login,
   onBackPress: dismissScreen,
 })(Login);
