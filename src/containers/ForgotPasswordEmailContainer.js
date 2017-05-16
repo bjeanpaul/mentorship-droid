36da715340f94ebea8b2e1a8b27f6cb5f0d59b53
nathanbegbie <nathanbegbie@gmail.com>
2017-05-16 11:58:53 +0200

    Implement ForgotPasswordEmail container and view

--- src/containers/ForgotPasswordEmailContainer.js
+++ src/containers/ForgotPasswordEmailContainer.js
@@ -2,10 +2,12 @@ import { connect } from 'react-redux';
 
 import ForgotPasswordEmail from 'src/views/ForgotPasswordEmail';
 import { dismissScreen } from 'src/actions/navigation';
+import { emailForgotPasswordToken } from 'src/actions/forgotPassword';
 
 
 export const mapDispatchToProps = {
   onDismissPress: dismissScreen,
+  onSendEmailPress: emailForgotPasswordToken,
 };
 
 
--- src/views/ForgotPasswordEmail/index.js
+++ src/views/ForgotPasswordEmail/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import NotYetImplemented from 'src/views/NotYetImplemented';
+import React, { PropTypes } from 'react';
+import { FormView, Text, Header, HeaderIcon, Button, TextInput } from 'src/components';
 
 
-const ForgotPasswordEmail = props => (
-  <NotYetImplemented {...props} />
-);
+class ForgotPasswordEmail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+  }
+
+  render() {
+    return (
+      <FormView>
+        <Header>
+          <Text style={Text.types.title}>Send Password Reminder</Text>
+
+          <HeaderIcon
+            uid="dismiss"
+            type={HeaderIcon.types.backDark}
+            onPress={this.props.onDismissPress}
+          />
+        </Header>
+
+        <TextInput
+          label="Email"
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+        />
+
+        <Button
+          uid="sendEmail"
+          disabled={this.state.email === '' }
+          onPress={() => this.props.onSendEmailPress(
+            this.state.email
+          )}
+        >
+          Send Email
+        </Button>
+      </FormView>
+    );
+  }
+}
+
+ForgotPasswordEmail.propTypes = {
+  onDismissPress: PropTypes.func.isRequired,
+  onSendEmailPress: PropTypes.func.isRequired,
+};
 
 
 export default ForgotPasswordEmail;
