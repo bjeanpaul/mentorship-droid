e506764b27243a676441501253b04f0110ef2668
nathanbegbie <nathanbegbie@gmail.com>
2017-05-16 11:56:01 +0200

    Add forgot password navigation reducers to navigation reducer

--- src/reducers/navigation/forgotPassword.js
+++ src/reducers/navigation/forgotPassword.js
@@ -1,5 +1,14 @@
+import * as routes from 'src/constants/routes';
+import * as forgotPassword from 'src/constants/forgotPassword';
+import { push, createRoute } from 'src/navigationHelpers';
+
+
 export default (state, action) => {
   switch (action.type) {
+    case forgotPassword.SHOW_FORGOT_PASSWORD_EMAIL: {
+      return push(state, createRoute(routes.ROUTE_FORGOT_PASSWORD_EMAIL));
+    }
+
     default:
       return state;
   }
