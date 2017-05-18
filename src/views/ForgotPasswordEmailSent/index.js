66741254f8c712157534e1df5b3ab9df89b127a7
nathanbegbie <nathanbegbie@gmail.com>
2017-05-18 09:19:42 +0200

    Initial implementation of Email Sent screen for forgot Password

--- src/views/ForgotPasswordEmailSent/index.js
+++ src/views/ForgotPasswordEmailSent/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import NotYetImplemented from 'src/views/NotYetImplemented';
+import React, { PropTypes } from 'react';
+import { Image, View } from 'react-native';
 
+import images from 'src/constants/images';
+import { BaseView, Header, HeaderIcon, Text, Button } from 'src/components';
+import styles from 'src/views/ForgotPasswordEmailSent/styles';
 
-const ForgotPasswordEmailSent = props => (
-  <NotYetImplemented {...props} />
+const ForgotPasswordEmailSent = ({
+  onDismissPress,
+  onLoginPress,
+}) => (
+  <BaseView style={styles.base}>
+    <Header>
+      {
+        onDismissPress && <HeaderIcon
+          uid="dismiss"
+          type={HeaderIcon.types.dismissLight}
+          onPress={onDismissPress}
+        />
+      }
+    </Header>
+
+    <View style={styles.body}>
+      <View style={styles.contentContainer}>
+        <Image source={images.OVERLAY_DONE} />
+        <Text style={styles.title}>Please check your email</Text>
+
+        <Text style={[Text.themes.light, styles.message]}>
+          We sent you an email with instructions for resetting your password
+        </Text>
+      </View>
+
+      <View style={styles.actionContainer}>
+        <Button
+          uid="logIn"
+          theme={Button.themes.transparentDark}
+          onPress={onLoginPress}
+        >
+          LOG IN
+        </Button>
+      </View>
+    </View>
+  </BaseView>
 );
+
+ForgotPasswordEmailSent.PropTypes = {
+  onDismissPress: PropTypes.func.isRequired,
+};
 
 
 export default ForgotPasswordEmailSent;
