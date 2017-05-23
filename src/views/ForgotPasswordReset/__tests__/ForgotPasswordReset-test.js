1c661c9716a9863d21aba4e87f50b2f8c869b51a
nathanbegbie <nathanbegbie@gmail.com>
2017-05-23 17:11:08 +0200

    Update tests for forgotPassword views

--- src/views/ForgotPasswordReset/__tests__/ForgotPasswordReset-test.js
+++ src/views/ForgotPasswordReset/__tests__/ForgotPasswordReset-test.js
@@ -1,14 +1,21 @@
 import { noop } from 'lodash';
 import React from 'react';
-
+// import { uidEquals } from 'app/scripts/helpers';
 import ForgotPasswordReset from 'src/views/ForgotPasswordReset';
 
+import {
+  forgotPasswordResetStatusIdle,
+  forgotPasswordResetStatusBusy,
+  forgotPasswordResetStatusBadToken,
+} from 'src/statuses/forgotPassword';
 
 describe('ForgotPasswordReset', () => {
   function createComponent(props = {}) {
     return (
       <ForgotPasswordReset
-        onDismissPress={noop}
+        onBackPress={noop}
+        onLoginPress={noop}
+        status={forgotPasswordResetStatusIdle()}
         {...props}
       />
     );
@@ -18,6 +25,51 @@ describe('ForgotPasswordReset', () => {
     expect(render(createComponent())).toMatchSnapshot();
   });
 
-  // TODO - tests for status
-  // TODO - tests for button enabled/disabled
+  it('should render as busy when status is busy', () => {
+    const status = forgotPasswordResetStatusBusy();
+    const el = createComponent({
+      status,
+    });
+
+    expect(render(el)).toMatchSnapshot();
+  });
+
+  it('should render password token error', () => {
+    const status = forgotPasswordResetStatusBadToken();
+    const el = createComponent({
+      status,
+    });
+
+    expect(render(el)).toMatchSnapshot();
+  });
+
+  // it('should call onBackPress', () => {
+  //   const onBackPress = jest.fn();
+  //   const el = shallow(createComponent({ onBackPress }));
+
+  //   el.findWhere(uidEquals('back'))
+  //     .simulate('press');
+
+  //   expect(onBackPress).toBeCalled();
+  // });
+
+  it(('should display the password mismatch error message '
+      + 'when passwords are mismatched'), () => {
+    const el = render(createComponent());
+    el.getInstance().setState({
+      newPassword: 'foo',
+      checkNewPassword: 'f',
+    });
+    expect(el.toJSON()).toMatchSnapshot();
+  });
+
+  it(('should not display the password mismatch error message '
+      + 'when passwords are matched'), () => {
+    const el = render(createComponent());
+    el.getInstance().setState({
+      newPassword: 'foo',
+      checkNewPassword: 'foo',
+    });
+    expect(el.toJSON()).toMatchSnapshot();
+  });
 });
